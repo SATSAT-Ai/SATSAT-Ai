96b509e29216522a48618ce241f9857f8eac5484
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-10-31 11:02:57 +0000

    update navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
 
 	useEffect(() => {
 		const handleScroll = () => {
-			if (window.scrollY > 60) {
+			if (window.scrollY > 70) {
 				setScrolled(true);
 			} else {
 				setScrolled(false);
@@ -29,76 +29,78 @@ const Header = () => {
 	}, []);
 
 	return (
-		<header
-			className={`py-3 z-10 fixed w-full transition-background duration-300 ${
-				scrolled ? "bg-white/10 backdrop-blur-md saturate-150" : ""
-			} top-0`}
-		>
-			<div className="my-max flex items-center justify-between w-full">
-				<Link href={"/"}>
-					<Image src={satsatLogo} height={120} width={120} alt="SATSAT-Ai" />
-				</Link>
-				<MdMenu
-					className="sm:hidden cursor-pointer"
-					color="white"
-					size="25"
-					onClick={() => setShowNav(true)}
-				/>
-				{showNav && (
-					<div className="sm:hidden">
-						<MobileNav setShowNav={setShowNav} />
-					</div>
-				)}
-				<ul className="hidden sm:flex items-center gap-5 font-normal text-text-normal">
-					<li
-						className={`${
-							pathname == "/"
-								? "text-mid--yellow"
-								: "text-grey-lightest hover:text-white"
-						}`}
-					>
-						<Link href={"/home"}>Home</Link>
-					</li>
-					<li
-						className={`${
-							pathname == "/about"
-								? "text-mid--yellow"
-								: "text-grey-lightest hover:text-white"
-						}`}
-					>
-						<Link href={"/about"}>About</Link>
-					</li>
-					<li
-						className={`${
-							pathname == "/features"
-								? "text-mid--yellow"
-								: "text-grey-lightest hover:text-white"
-						}`}
-					>
-						<Link href={"/features"}>Features</Link>
-					</li>
-					<li
-						className={`${
-							pathname == "/contact"
-								? "text-mid--yellow"
-								: "text-grey-lightest hover:text-white"
-						}`}
-					>
-						<Link href={"/contact"}>Contact</Link>
-					</li>
-					<li className="text-grey-lightest hover:text-white">
-						<Link href={"/login"}>Sign in</Link>
-					</li>
-					<Link
-						className="flex items-center gap-3 hover:bg-mid--yellow transition-colors duration-200 active:scale-[1.01] text-white bg-brand-green button"
-						href={"/signup"}
-					>
-						Get Started
-						<MdArrowForward color="white" size="25" />
-					</Link>
-				</ul>
+		<>
+			<div className={`sm:hidden`}>
+				<MobileNav setShowNav={setShowNav} showNav={showNav} />
 			</div>
-		</header>
+			<header
+				className={`py-3 z-10 fixed w-full transition-background duration-300 ${
+					scrolled
+						? "bg-white/10 backdrop-blur-md saturate-150"
+						: "bg-none backdrop-blur-none"
+				} top-0`}
+			>
+				<div className="my-max flex items-center justify-between w-full">
+					<Link href={"/"}>
+						<Image src={satsatLogo} height={100} width={100} alt="SATSAT-Ai" />
+					</Link>
+					<MdMenu
+						className="sm:hidden ml-auto cursor-pointer"
+						color="white"
+						size="25"
+						onClick={() => setShowNav(true)}
+					/>
+					<ul className="hidden sm:flex items-center gap-5 font-normal text-text-normal">
+						<li
+							className={`${
+								pathname == "/"
+									? "text-mid--yellow font-medium"
+									: "text-grey-lightest hover:text-white"
+							}`}
+						>
+							<Link href={"/"}>Home</Link>
+						</li>
+						<li
+							className={`${
+								pathname == "/about"
+									? "text-mid--yellow font-medium"
+									: "text-grey-lightest hover:text-white"
+							}`}
+						>
+							<Link href={"/about"}>About</Link>
+						</li>
+						<li
+							className={`${
+								pathname == "/features"
+									? "text-mid--yellow font-medium"
+									: "text-grey-lightest hover:text-white"
+							}`}
+						>
+							<Link href={"/features"}>Features</Link>
+						</li>
+						<li
+							className={`${
+								pathname == "/contact"
+									? "text-mid--yellow font-medium"
+									: "text-grey-lightest hover:text-white"
+							}`}
+						>
+							<Link href={"/contact"}>Contact</Link>
+						</li>
+						<li className="text-grey-lightest hover:text-white">
+							<Link href={"/login"}>Sign in</Link>
+						</li>
+						<Link
+							className="flex items-center gap-3 font-normal hover:bg-mid--yellow transition-colors duration-200 active:scale-[1.01] text-white bg-brand-green button"
+							href={"/signup"}
+						>
+							Get Started
+							<MdArrowForward color="white" size="25" />
+						</Link>
+					</ul>
+				</div>
+			</header>
+		</>
 	);
 };
 
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -5,62 +5,80 @@ import { SetStateAction, Dispatch } from "react";
 
 interface navProps {
 	setShowNav: Dispatch<SetStateAction<boolean>>;
+	showNav: boolean;
 }
 
-const MobileNav = ({ setShowNav }: navProps) => {
+const MobileNav = ({ setShowNav, showNav }: navProps) => {
 	const pathname = usePathname();
 
 	return (
-		<ul className="flex flex-col fixed top-0 right-0 w-3/4 bg-brand-green-darker z-50 h-screen justify-center items-center gap-5 font-medium text-[17px]">
-			<MdClose
-				onClick={() => setShowNav(false)}
-				color="white"
-				size="25"
-				className="absolute cursor-pointer top-5 right-4"
-			/>
-			<li
-				className={`${
-					pathname == "/"
-						? "text-mid--yellow"
-						: "text-grey-lightest hover:text-white"
-				}`}
+		<>
+			{
+				<div
+					onClick={() => setShowNav(false)}
+					className={`  ${
+						showNav
+							? "translate-x-0"
+							: " translate-x-full delay-100 duration-100"
+					} bg-white/10 duration-150 motio saturate-150 fixed backdrop-blur-lg top-0 z-50 left-0 h-screen w-full`}
+				></div>
+			}
+			<ul
+				className={` ${
+					showNav ? "delay-100 translate-x-0" : " translate-x-full"
+				}
+					 flex z-50 overflow-y-scroll scrollbar-hidden flex-col transition-transform duration-100  fixed top-0 right-0 w-3/4 bg-brand-green-darker/90 h-screen justify-center items-center gap-5 font-medium text-[17px]`}
 			>
-				<Link href={"/home"}>Home</Link>
-			</li>
-			<li
-				className={`${
-					pathname == "/about"
-						? "text-mid--yellow"
-						: "text-grey-lightest hover:text-white"
-				}`}
-			>
-				<Link href={"/about"}>About</Link>
-			</li>
-			<li
-				className={`${
-					pathname == "/features"
-						? "text-mid--yellow"
-						: "text-grey-lightest hover:text-white"
-				}`}
-			>
-				<Link href={"/features"}>Features</Link>
-			</li>
-			<li
-				className={`${
-					pathname == "/contact"
-						? "text-mid--yellow"
-						: "text-grey-lightest hover:text-white"
-				}`}
-			>
-				<Link href={"/contact"}>Contact</Link>
-			</li>
-			<li className="text-grey-lightest hover:text-white">
-				<Link href={"/login"}>Sign in</Link>
-			</li>
-			<Link className="text-white bg-brand-green button" href={"/signup"}>
-				Get Started
-			</Link>
-		</ul>
+				<MdClose
+					onClick={() => setShowNav(false)}
+					color="white"
+					size="25"
+					className="absolute cursor-pointer top-5 right-4"
+				/>
+				<li
+					className={`${
+						pathname == "/"
+							? "text-mid--yellow"
+							: "text-grey-lightest hover:text-white"
+					}`}
+				>
+					<Link href={"/"}>Home</Link>
+				</li>
+				<li
+					className={`${
+						pathname == "/about"
+							? "text-mid--yellow"
+							: "text-grey-lightest hover:text-white"
+					}`}
+				>
+					<Link href={"/about"}>About</Link>
+				</li>
+				<li
+					className={`${
+						pathname == "/features"
+							? "text-mid--yellow"
+							: "text-grey-lightest hover:text-white"
+					}`}
+				>
+					<Link href={"/features"}>Features</Link>
+				</li>
+				<li
+					className={`${
+						pathname == "/contact"
+							? "text-mid--yellow"
+							: "text-grey-lightest hover:text-white"
+					}`}
+				>
+					<Link href={"/contact"}>Contact</Link>
+				</li>
+				<li className="text-grey-lightest hover:text-white">
+					<Link href={"/login"}>Sign in</Link>
+				</li>
+				<Link className="text-white bg-brand-green button" href={"/signup"}>
+					Get Started
+				</Link>
+			</ul>
+		</>
 	);
 };
 
