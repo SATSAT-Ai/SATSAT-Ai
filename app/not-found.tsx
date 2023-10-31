3b787738083cf08824ba2414094caa1655323289
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-10-31 01:50:45 +0000

    update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,9 +5,12 @@ import Link from "next/link";
 const NotFound = () => {
 	return (
 		<main className="bg-darker min-h-screen w-full relative pb-20">
-			<div className="flex items-center justify-center w-full mb-5 h-full pt-20">
-				<Image src={NotfoundImage} width={250} height={300} alt="404" />
+			<div className="flex items-center justify-center w-full h-full pt-20">
+				<Image src={NotfoundImage} width={200} height={300} alt="404" />
 			</div>
+			<p className="text-mid--yellow text-center py-5">
+				The page you are looking for cannot be found.
+			</p>
 			<Link
 				href="/"
 				className=" active:scale-[1.01] button w-fit hover:bg-brand-green border border-white block mx-auto transition-all text-white hover:border-none"
