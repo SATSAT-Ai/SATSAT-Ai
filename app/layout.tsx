b23b282095b4cd8c6df43e28c5b6b8b27e403e00
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-04 23:46:40 +0000

    install react-top-loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Roboto } from "next/font/google";
 import "./globals.scss";
 import { SATSATmetadata } from "@/utils/metadata";
 
+import NextTopLoader from "nextjs-toploader";
+
 const roboto = Roboto({
 	subsets: ["latin", "greek"],
 	weight: ["300", "400", "700", "900"],
@@ -17,7 +19,20 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={roboto.className}>{children}</body>
+			<body className={roboto.className}>
+				<NextTopLoader
+					showSpinner={true}
+					color="#29a173"
+					initialPosition={0.04}
+					crawlSpeed={300}
+					height={3}
+					crawl={true}
+					easing="ease"
+					speed={200}
+					shadow="0 0 10px #29a173,0 0 5px #29a173"
+				/>
+				{children}
+			</body>
 		</html>
 	);
 }
