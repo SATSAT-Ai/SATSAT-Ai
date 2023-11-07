8e748ae60cb4fcdae6f777325d8b5aeaaf31a6aa
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-07 16:56:13 +0000

    add loading ui

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import { MoonLoader } from "react-spinners";
 const Loading = () => {
 	return (
 		<div className="min-h-screen w-full bg-darker text-white flex flex-col items-center justify-center">
-			<h1 className="text-text-24">Loading...</h1>
+			{/* <h1 className="text-text-24">Loading...</h1> */}
 			<MoonLoader color="#29a173" />
 		</div>
 	);
