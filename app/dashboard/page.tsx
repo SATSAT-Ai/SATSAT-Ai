1ee8ea974965ea410a19ba6676f228cb6d4078d9
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-07 17:03:03 +0000

    udpate dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Footer from "@/components/Footer";
-import React from "react";
 
 const page = () => {
-	return <div className="min-h-screen">page</div>;
+	return (
+		<div className="min-h-screen text-white p-5">
+			<h1 className="text-text-40">Dashboard</h1>
+			<div className="flex items-center"></div>
+		</div>
+	);
 };
 
 export default page;
