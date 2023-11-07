385277f11f69114f8514831c9fc5195f306024fa
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-07 16:56:50 +0000

    add DashboardSidebar

--- components/DashboardSidebar.tsx
+++ components/DashboardSidebar.tsx
@@ -3,18 +3,21 @@ import Image from "next/image";
 import Logo from "@/public/satsat-logo.svg";
 import DashboardData from "@/app/dashboard/dashboardData";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const DashboardSidebar = () => {
 	const pathname = usePathname();
 	return (
-		<div className="bg-white/10 backdrop-blur-sm text-white h-full">
-			<Image
-				className="mx-auto w-fit pt-4"
-				src={Logo}
-				height={110}
-				width={110}
-				alt="SATSAT-Ai"
-			/>
+		<div className="bg-white/10 backdrop-blur-md text-white h-full">
+			<Link href={"/"}>
+				<Image
+					className="mx-auto w-fit pt-4"
+					src={Logo}
+					height={110}
+					width={110}
+					alt="SATSAT-Ai"
+				/>
+			</Link>
 			<DashboardData pathname={pathname} />
 		</div>
 	);
