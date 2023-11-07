b65166127180ec27ef233da25c35f5d17b732054
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-07 18:45:44 +0000

    update dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import DashboardSidebar from "@/components/DashboardSidebar";
-import Footer from "@/components/Footer";
 import { ReactNode } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme";
@@ -9,7 +8,6 @@ import ContextProvider from "@/context/AppContext";
 const layout = ({ children }: { children: ReactNode }) => {
 	return (
 		<ThemeProvider theme={theme}>
-			{/* dashboardbg */}
 			<div className="flex relative bg-darker min-h-screen ">
 				<div className="green-blob w-96 h-96 top-[-50%] md:top-[10%] left-[35%]"></div>
 				<div className="yellow-blob w-96 h-96 top-[-10%] md:top-[0%] right-[0%]"></div>
@@ -20,7 +18,6 @@ const layout = ({ children }: { children: ReactNode }) => {
 				<div className="flex-[10]">
 					<DashboardHeader />
 					<ContextProvider>{children}</ContextProvider>
-					{/* <Footer /> */}
 				</div>
 			</div>
 		</ThemeProvider>
