c1d714ff84facdbd47df7e284280e3750a2692c1
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-07 16:57:09 +0000

    add SATSAT-Ai metadata

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -1,19 +1,19 @@
-import logo from "../public/satsat-logo.svg";
 import icon from "../public/satsat-logo.svg";
 import openGraphImage from "../public/prompt-page-large.jpg";
+import { Metadata } from "next";
 
-export const SATSATmetadata = {
+export const SATSATmetadata: Metadata = {
 	title: "SATSAT-Ai",
 	description:
 		"SATSAT Ai is an all-in-one artificial intelligence platform that combines cutting-edge AI technology to query your financial data with natural language.",
 	applicationName: "SATSAT Ai",
-	icons: icon,
+	icons: [icon],
 
 	authors: [
 		{ name: "Kamasah Dickson", url: "" },
 		{ name: "Emmanuel Jean Kamasah", url: "" },
 	],
-	keywords: ["Nextjs", "React", "Server components"],
+	keywords: ["Nextjs", "React", "Server components", "SATSAT-Ai"],
 	creator: "Kamasah Dickson",
 
 	openGraph: {
@@ -23,8 +23,9 @@ export const SATSATmetadata = {
 		siteName: "SATSAT-Ai",
 		description:
 			"SATSAT Ai is an all-in-one artificial intelligence platform that combines cutting-edge AI technology to query your financial data with natural language.",
-		images: [logo, openGraphImage],
+		images: [openGraphImage, icon],
 	},
+
 	twitter: {
 		card: "summary_large_image",
 		title: "SATSAT-Ai",
@@ -33,6 +34,7 @@ export const SATSATmetadata = {
 		images: [openGraphImage as unknown as string],
 		creator: "@jeanlinux",
 	},
+	metadataBase: new URL(process.env.WEBSITE_URL as string),
 
 	alternates: {
 		canonical: process.env.WEBSITE_URL,
