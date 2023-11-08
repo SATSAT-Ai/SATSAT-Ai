9b70431b33359de01981f38ba729584b8184b168
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-08 00:59:50 +0000

    update sidebar

--- components/DashboardSidebar.tsx
+++ components/DashboardSidebar.tsx
@@ -4,12 +4,19 @@ import Logo from "@/public/satsat-logo.svg";
 import DashboardData from "@/app/dashboard/dashboardData";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useContext } from "react";
+import { AppContext } from "@/context/AppContext";
 
 const DashboardSidebar = () => {
 	const pathname = usePathname();
+	const { hideSidebar, setHideSidebar } = useContext(AppContext);
 	return (
-		<div className="bg-white/10 backdrop-blur-md text-white h-full">
-			<Link href={"/"}>
+		<div
+			className={`bg-white/10 backdrop-blur-md text-white min-h-full relative flex-grow-0 flex-shrink-0 md:flex-[150px] ${
+				hideSidebar ? "hidden" : "block"
+			}`}
+		>
+			<Link href={"/"} className="hidden md:flex">
 				<Image
 					className="mx-auto w-fit pt-4"
 					src={Logo}
@@ -18,7 +25,8 @@ const DashboardSidebar = () => {
 					alt="SATSAT-Ai"
 				/>
 			</Link>
-			<DashboardData pathname={pathname} />
+
+			<DashboardData setHideSidebar={setHideSidebar} pathname={pathname} />
 		</div>
 	);
 };
