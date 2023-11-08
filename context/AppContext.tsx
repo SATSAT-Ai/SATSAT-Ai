4bc2e2e70a9b4f24a1731380a5fa592c524192aa
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-08 00:56:56 +0000

    update global context

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -5,24 +5,36 @@ import { Dispatch, SetStateAction, createContext, useState } from "react";
 interface Icontext {
 	showModal: boolean;
 	modalAction: string;
+	hideSidebar: boolean;
 	setShowModal: Dispatch<SetStateAction<boolean>>;
 	setModalAction: Dispatch<SetStateAction<string>>;
+	setHideSidebar: Dispatch<SetStateAction<boolean>>;
 }
 
 export const AppContext = createContext<Icontext>({
 	showModal: false,
 	modalAction: "deleteAccount",
+	hideSidebar: false,
 	setShowModal: () => {},
 	setModalAction: () => {},
+	setHideSidebar: () => {},
 });
 
 const ContextProvider = ({ children }: { children: React.ReactNode }) => {
 	const [showModal, setShowModal] = useState(false);
 	const [modalAction, setModalAction] = useState("deleteAccount");
+	const [hideSidebar, setHideSidebar] = useState(false);
 
 	return (
 		<AppContext.Provider
-			value={{ showModal, setShowModal, setModalAction, modalAction }}
+			value={{
+				showModal,
+				setShowModal,
+				setModalAction,
+				modalAction,
+				setHideSidebar,
+				hideSidebar,
+			}}
 		>
 			{children}
 		</AppContext.Provider>
