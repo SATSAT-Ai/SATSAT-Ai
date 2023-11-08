0c3215f0b107ed78fb9e07e9d099dcc3720a4ccb
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-08 00:57:58 +0000

    udpate dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,17 +8,17 @@ import ContextProvider from "@/context/AppContext";
 const layout = ({ children }: { children: ReactNode }) => {
 	return (
 		<ThemeProvider theme={theme}>
-			<div className="flex relative bg-darker min-h-screen ">
-				<div className="green-blob w-96 h-96 top-[-50%] md:top-[10%] left-[35%]"></div>
-				<div className="yellow-blob w-96 h-96 top-[-10%] md:top-[0%] right-[0%]"></div>
+			<div className="flex relative bg-darker min-h-screen">
+				<div className="green-blob !fixed w-96 h-96 top-[-50%] md:top-[10%] left-[35%]"></div>
+				<div className="yellow-blob w-96 !fixed h-96 top-[-10%] md:top-[0%] right-[0%]"></div>
 
-				<div className="flex-grow-0 flex-shrink-0 flex-[255px]">
+				<ContextProvider>
 					<DashboardSidebar />
-				</div>
-				<div className="flex-[10]">
-					<DashboardHeader />
-					<ContextProvider>{children}</ContextProvider>
-				</div>
+					<div className="flex-[10]">
+						<DashboardHeader />
+						{children}
+					</div>
+				</ContextProvider>
 			</div>
 		</ThemeProvider>
 	);
