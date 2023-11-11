21cc3110d7aa49c4064c1315658cd9f54557752d
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-11 17:27:40 +0000

    add dashboard sidebar

--- components/DashboardSidebar.tsx
+++ components/DashboardSidebar.tsx
@@ -1,9 +1,7 @@
 "use client";
-import Image from "next/image";
-import Logo from "@/public/satsat-logo.svg";
+
 import DashboardData from "@/app/dashboard/dashboardData";
 import { usePathname } from "next/navigation";
-import Link from "next/link";
 import { useContext } from "react";
 import { AppContext } from "@/context/AppContext";
 
@@ -12,20 +10,10 @@ const DashboardSidebar = () => {
 	const { hideSidebar, setHideSidebar } = useContext(AppContext);
 	return (
 		<div
-			className={`bg-white/10 backdrop-blur-md text-white min-h-full relative flex-grow-0 flex-shrink-0 md:flex-[150px] ${
+			className={`bg-white/10 backdrop-blur-md text-white min-h-full relative flex-initial md:basis-64 ${
 				hideSidebar ? "hidden" : "block"
 			}`}
 		>
-			<Link href={"/"} className="hidden md:flex">
-				<Image
-					className="mx-auto w-fit pt-4"
-					src={Logo}
-					height={110}
-					width={110}
-					alt="SATSAT-Ai"
-				/>
-			</Link>
-
 			<DashboardData setHideSidebar={setHideSidebar} pathname={pathname} />
 		</div>
 	);
