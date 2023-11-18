502b5b906943f112f8a2df0b4183e9a3826453ec
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-18 02:28:03 +0000

    update layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,26 +1,31 @@
 import DashboardSidebar from "@/components/DashboardSidebar";
 import { ReactNode } from "react";
 import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./theme";
 import DashboardHeader from "@/components/DashboardHeader";
 import ContextProvider from "@/context/AppContext";
 
+import { theme } from "./theme";
+
 const layout = ({ children }: { children: ReactNode }) => {
 	return (
-		<ThemeProvider theme={theme}>
+		<>
 			<div className="flex relative bg-darker min-h-screen">
 				<div className="green-blob !fixed w-96 h-96 top-[-50%] md:top-[10%] left-[35%]"></div>
 				<div className="yellow-blob w-96 !fixed h-96 top-[-10%] md:top-[0%] right-[0%]"></div>
 
 				<ContextProvider>
-					<DashboardSidebar />
-					<div className="flex-[10]">
-						<DashboardHeader />
+					<ThemeProvider theme={theme}>
+						<DashboardSidebar />
+					</ThemeProvider>
+					<div className="flex-[10] z-10">
+						<ThemeProvider theme={theme}>
+							<DashboardHeader />
+						</ThemeProvider>
 						{children}
 					</div>
 				</ContextProvider>
 			</div>
-		</ThemeProvider>
+		</>
 	);
 };
 
