b3823c96d56e84999d7682330189d6ef233eb571
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-18 02:21:30 +0000

    updated sidebar

--- components/DashboardSidebar.tsx
+++ components/DashboardSidebar.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import DashboardData from "@/app/dashboard/dashboardData";
 import { usePathname } from "next/navigation";
 import { useContext } from "react";
 import { AppContext } from "@/context/AppContext";
+import DashboardData from "./DashboardData";
 
 const DashboardSidebar = () => {
 	const pathname = usePathname();
 	const { hideSidebar, setHideSidebar } = useContext(AppContext);
 	return (
 		<div
-			className={`bg-white/10 backdrop-blur-md text-white min-h-full relative flex-initial md:basis-64 ${
+			className={`bg-white/10 backdrop-blur-md text-white min-h-full z-40 sm:relative absolute flex-initial md:basis-64 ${
 				hideSidebar ? "hidden" : "block"
 			}`}
 		>
