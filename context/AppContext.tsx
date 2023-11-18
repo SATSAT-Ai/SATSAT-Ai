0a4faecafcb2491405044f94fcdfefaae62836ac
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-18 02:24:06 +0000

    update appContext

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -6,24 +6,29 @@ interface Icontext {
 	showModal: boolean;
 	modalAction: string;
 	hideSidebar: boolean;
+	showNotification: boolean;
 	setShowModal: Dispatch<SetStateAction<boolean>>;
 	setModalAction: Dispatch<SetStateAction<string>>;
 	setHideSidebar: Dispatch<SetStateAction<boolean>>;
+	setShowNotification: Dispatch<SetStateAction<boolean>>;
 }
 
 export const AppContext = createContext<Icontext>({
 	showModal: false,
 	modalAction: "deleteAccount",
 	hideSidebar: false,
+	showNotification: false,
 	setShowModal: () => {},
 	setModalAction: () => {},
 	setHideSidebar: () => {},
+	setShowNotification: () => {},
 });
 
 const ContextProvider = ({ children }: { children: React.ReactNode }) => {
 	const [showModal, setShowModal] = useState(false);
 	const [modalAction, setModalAction] = useState("deleteAccount");
 	const [hideSidebar, setHideSidebar] = useState(false);
+	const [showNotification, setShowNotification] = useState(false);
 
 	return (
 		<AppContext.Provider
@@ -34,6 +39,8 @@ const ContextProvider = ({ children }: { children: React.ReactNode }) => {
 				modalAction,
 				setHideSidebar,
 				hideSidebar,
+				showNotification,
+				setShowNotification,
 			}}
 		>
 			{children}
