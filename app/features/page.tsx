f2c4dad51d10cc359e040c567e455cf974cb5262
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-18 02:30:00 +0000

    update feature page

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -14,8 +14,9 @@ const Features = () => {
 		<>
 			<Header />
 
-			<main className="bg-darker min-h-screen w-full py-10 relative">
-				<div className="green-blob w-96 h-96 top-[-50%] md:top-[-25%] left-[50%]"></div>
+			<main className="bg-darker min-h-screen w-full py-10 relative overflow-hidden">
+				<div className="green-blob w-96 h-96 animate-pulse top-[-10%] md:top-[-20%] left-[50%]"></div>
+
 				<div className="my-max">
 					<h1 className="capitalize mt-16 text-brand-green text-center max-w-5xl mx-auto text-text-40 md:text-text-60 lg:text-text-60">
 						Features
@@ -29,14 +30,17 @@ const Features = () => {
 									className="text-white bg-brand-green-darker p-4 rounded-2xl"
 								>
 									{data.icon}
-									<h2 className="text-text-20 font-normal mt-2">
+									<h2 className="text-text-20 font-medium mt-2">
 										{data.title}
 									</h2>
 									<p className="mb-3 mt-1 text-white/80 font-normal text-text-normal">
 										{data.para}
 									</p>
-									<Link className="text-mid--yellow" href={data.link}>
-										<span>Learn More</span>
+									<Link
+										className="text-mid--yellow visited:text-brand-green"
+										href={data.link}
+									>
+										Learn More
 									</Link>
 								</div>
 							);
