b1deda50416b4750ab3ac8c49fb017a2cd2bb645
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-24 02:09:22 +0000

    update loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,9 +3,9 @@ import { MoonLoader } from "react-spinners";
 
 const Loading = () => {
 	return (
-		<div className="min-h-screen w-full bg-darker text-white flex flex-col items-center justify-center">
-			<h1 className="text-text-24">Loading...</h1>
+		<div className="min-h-screen w-full bg-darker text-white flex gap-5 flex-col items-center justify-center">
 			<MoonLoader color="#29a173" />
+			<h1 className="text-text-24">Loading...</h1>
 		</div>
 	);
 };
