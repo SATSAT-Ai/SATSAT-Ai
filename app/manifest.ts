b0f92bd619dcea559b6cf085d0c6f17024f92d8c
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-24 02:10:04 +0000

    update manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,7 +4,7 @@ import icon from "./icon.svg";
 export default function manifest(): MetadataRoute.Manifest {
 	return {
 		name: "SATSAT-AI",
-		short_name: "SATSAT-Ai",
+		short_name: "SATSAT-AI",
 		description:
 			"SATSAT AI is an all-in-one artificial intelligence platform that combines cutting-edge AI technology to query your financial data with natural language.",
 		start_url: "/",
