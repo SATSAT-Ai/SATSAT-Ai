c8558699d3748ce5795ec086ba84c5c6ef0ade27
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-24 02:08:40 +0000

    add about

---
+++ app/about/page.tsx
@@ -0,0 +1,110 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+const page = () => {
+	return (
+		<>
+			<Header />
+
+			<main className="bg-darker min-h-screen w-full py-10 relative overflow-hidden">
+				<div className="green-blob w-96 h-96 animate-pulse top-[-10%] md:top-[-20%] left-[50%]"></div>
+
+				<div className="my-max">
+					<h1 className="capitalize mt-16 text-brand-green text-center max-w-5xl mx-auto text-text-40 md:text-text-60 lg:text-text-60">
+						About us
+					</h1>
+					<div className="flex text-text-normal font-normal text-grey-lightest items-center flex-col gap-3">
+						<p>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur
+							molestias officia ad et quos quia eligendi dolorum, eos error
+							nostrum iusto odit! Consequatur aliquam impedit enim omnis
+							excepturi ea aut illo ratione veniam. Pariatur quidem id qui,
+							quibusdam ipsum error dicta eveniet nam modi repellendus suscipit
+							ipsa omnis laborum doloribus incidunt? Voluptatem alias sit
+							suscipit reprehenderit incidunt, fugiat nihil at quod quos
+							adipisci et fugit aliquam cum beatae mollitia vitae exercitationem
+							animi dolore corporis, error, iusto maiores possimus pariatur
+							ducimus? Numquam, obcaecati. Maxime sequi voluptas placeat
+							repellat cum necessitatibus voluptate veritatis saepe cumque
+							tempora dolorum qui repellendus, similique doloribus quas. Ipsam
+							repellat accusamus perferendis. Debitis, inventore dolore repellat
+							illo voluptates iste repudiandae libero soluta maiores aliquid
+							tempora in quae quos neque commodi modi natus minus? Voluptate,
+							maiores modi temporibus quidem eveniet nesciunt magni dolorum
+							corporis minima delectus! Placeat accusamus laudantium dolor,
+							necessitatibus doloribus sed ut, culpa dicta inventore cumque
+							molestiae nostrum minima magnam sint maxime soluta corporis quod
+							odio eveniet. Quaerat est quo necessitatibus quidem provident
+							dolorem similique quos sapiente, suscipit sed ex doloribus libero
+							explicabo temporibus reprehenderit enim a neque fuga dolores.
+							Dolor quaerat provident facilis necessitatibus. Ratione eius
+							numquam assumenda, ex nesciunt tempora aliquam sequi temporibus
+							nam cumque?
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+							magnam ea quisquam, laudantium minus alias neque maxime, numquam
+							doloribus quibusdam voluptatem praesentium expedita. Deleniti,
+							repellendus, dolorem atque iure at doloremque sequi earum, eum
+							alias aspernatur saepe id similique! Placeat unde sit quod eos,
+							quasi, impedit ipsa, alias molestias maiores nam dolor quisquam
+							libero tempora et fugiat. Tempora vero nemo eaque saepe mollitia
+							incidunt dicta repudiandae soluta cum architecto inventore
+							explicabo ad, neque eos aperiam quibusdam itaque ut cupiditate!
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Temporibus nemo minima explicabo. Magnam veniam repudiandae
+							officiis sit consectetur quos, veritatis consequuntur! Reiciendis
+							velit inventore quo doloribus voluptatibus architecto consequatur.
+							Neque quod quo quaerat accusamus assumenda! Soluta quae quam
+							facilis aut aliquid, hic quos necessitatibus repellat laborum
+							obcaecati vitae, consequatur, animi blanditiis. Nostrum eligendi
+							vero iure necessitatibus reiciendis in itaque mollitia.
+							Consectetur odio laudantium natus nulla voluptatem dolore magnam
+							ullam, aliquid atque ducimus illum perspiciatis aut, magni fugit
+							beatae cum animi deserunt expedita maxime impedit placeat sequi
+							dignissimos? Unde nostrum voluptatem, suscipit totam culpa
+							repellendus possimus accusamus voluptas incidunt est! Dolorum
+							doloremque, ex amet sapiente perferendis accusantium animi
+							eligendi qui. Obcaecati earum incidunt consequuntur explicabo amet
+							veniam eaque commodi, est quia voluptate autem vero? At incidunt
+							necessitatibus recusandae ipsam est praesentium illo, dolor minima
+							enim iure accusantium quidem, culpa porro, nostrum dolore
+							excepturi mollitia veniam eaque! Tempore veritatis nostrum impedit
+							magni.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+							soluta perspiciatis sint obcaecati nulla quo nesciunt, sequi
+							excepturi nam. Temporibus, officiis amet. Nemo, nihil. Sit hic
+							consequatur a quasi qui, sunt omnis praesentium dolor delectus.
+							Earum inventore ipsum perferendis explicabo eveniet consequatur
+							iure eos iusto fugit, id illo fuga harum delectus sed, ab eligendi
+							repellendus deleniti reprehenderit cum necessitatibus aperiam ex
+							reiciendis dolorem cumque! Culpa beatae itaque voluptates
+							reiciendis tenetur, nobis nam sequi corporis ullam excepturi quae
+							voluptatem repudiandae totam magnam, blanditiis dicta dolores vel
+							accusantium cumque assumenda! Recusandae eos impedit, similique
+							non sit corporis maxime? Repudiandae ad dolore explicabo
+							dignissimos asperiores velit quibusdam at nemo repellat unde hic
+							laudantium doloremque vitae blanditiis, praesentium minima
+							quaerat? Cum assumenda ipsa esse ducimus quos amet quibusdam
+							impedit unde, modi vitae fugit fugiat velit praesentium dolor
+							corporis voluptatum. Accusantium harum minima eos nam neque
+							maiores facilis suscipit cum sequi. Reiciendis atque esse tenetur,
+							quas dolore velit impedit reprehenderit adipisci dolor maiores
+							animi quis at? Ratione quos omnis modi amet animi ab molestiae
+							aliquam illum dolorum sint temporibus nemo, iure illo. Eos ex
+							voluptates ea deleniti quisquam quo magnam fuga dolor
+							reprehenderit
+						</p>
+					</div>
+				</div>
+			</main>
+			<Footer />
+		</>
+	);
+};
+
+export default page;
