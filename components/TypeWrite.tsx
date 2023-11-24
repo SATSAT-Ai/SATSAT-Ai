0327e96b26803ea2c152485f786aef23e21529a9
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-24 02:08:15 +0000

    add typeWrite

---
+++ components/TypeWrite.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const TypeWrite = ({ text }: { text: string }) => {
+	const [typedText, setTypedText] = useState("");
+	const [cursorVisible, setCursorVisible] = useState(true);
+
+	useEffect(() => {
+		const typingInterval = setInterval(() => {
+			if (typedText !== text) {
+				setTypedText(typedText + text[typedText.length]);
+			} else {
+				clearInterval(typingInterval);
+				setCursorVisible(false);
+			}
+		}, 50);
+
+		return () => {
+			clearInterval(typingInterval);
+		};
+	}, [text, typedText]);
+
+	return (
+		<div className="flex items-center justify-center flex-wrap">
+			<span>{typedText}</span>
+			{cursorVisible && (
+				<span className={cursorVisible ? "animate-pulse text-mid--yellow" : ""}>
+					|
+				</span>
+			)}
+		</div>
+	);
+};
+
+export default TypeWrite;
