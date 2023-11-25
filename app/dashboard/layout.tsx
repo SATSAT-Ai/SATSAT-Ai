2e7ca8fb860f9c7705519044fdd9d1f145a03e43
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-25 02:01:00 +0000

    update dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,24 +8,22 @@ import { theme } from "./theme";
 
 const layout = ({ children }: { children: ReactNode }) => {
 	return (
-		<>
-			<div className="flex relative bg-darker min-h-screen">
-				<div className="green-blob !fixed w-96 h-96 top-[-50%] md:top-[10%] left-[35%]"></div>
-				<div className="yellow-blob w-96 !fixed h-96 top-[-10%] md:top-[0%] right-[0%]"></div>
+		<div className="flex relative bg-darker min-h-screen">
+			<div className="green-blob !fixed w-96 h-96 top-[-50%] md:top-[10%] left-[35%]"></div>
+			<div className="yellow-blob w-96 !fixed h-96 top-[-10%] md:top-[0%] right-[0%]"></div>
 
-				<ContextProvider>
+			<ContextProvider>
+				<ThemeProvider theme={theme}>
+					<DashboardSidebar />
+				</ThemeProvider>
+				<div className="flex-[10] z-10">
 					<ThemeProvider theme={theme}>
-						<DashboardSidebar />
+						<DashboardHeader />
 					</ThemeProvider>
-					<div className="flex-[10] z-10">
-						<ThemeProvider theme={theme}>
-							<DashboardHeader />
-						</ThemeProvider>
-						{children}
-					</div>
-				</ContextProvider>
-			</div>
-		</>
+					{children}
+				</div>
+			</ContextProvider>
+		</div>
 	);
 };
 
