10058ca07033742c6e1a126b6eae16521d06a20a
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-25 01:59:57 +0000

    add layout file in dashboard chat

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
 
-const layout = () => {
-	return <div>layout</div>;
+import { ThemeProvider } from "@emotion/react";
+import { ReactNode } from "react";
+import { theme } from "../dashboard/theme";
+import ContextProvider from "@/context/AppContext";
+import ChatSidebar from "@/components/ChatSidebar";
+
+const layout = ({ children }: { children: ReactNode }) => {
+	return (
+		<div className="flex w-full relative bg-darker min-h-screen">
+			<div className="green-blob !fixed w-96 h-96 top-[-50%] md:top-[10%] left-[35%]"></div>
+			<div className="yellow-blob w-96 !fixed h-96 top-[-10%] md:top-[0%] right-[0%]"></div>
+
+			<ContextProvider>
+				<ThemeProvider theme={theme}>
+					<ChatSidebar />
+				</ThemeProvider>
+				<div className="flex-[10] z-10">{children}</div>
+			</ContextProvider>
+		</div>
+	);
 };
 
 export default layout;
