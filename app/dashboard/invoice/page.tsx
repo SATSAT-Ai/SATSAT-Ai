fcfb08f67cc9c8a9ec8bca2510cab326f0bf9722
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-11-30 02:48:27 +0000

    add invoice page

---
+++ app/dashboard/invoice/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "SATSAT-AI Invoice",
+};
+const page = () => {
+	return <div>page</div>;
+};
+
+export default page;
---
+++ app/dashboard/invoice/receipts/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "SATSAT-AI receipts",
+};
+const page = () => {
+	return <div>page</div>;
+};
+
+export default page;
