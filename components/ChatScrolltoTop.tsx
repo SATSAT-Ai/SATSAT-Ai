e9fc4d6f1119e1eb9c11a2a4ede2d69545a36392
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-03 02:14:32 +0000

    add scroll to top button in chats

---
+++ components/ChatScrolltoTop.tsx
@@ -0,0 +1,19 @@
+import NorthIcon from "@mui/icons-material/North";
+
+const ChatScrolltoBottom = ({ scrolltoTop }: { scrolltoTop: () => void }) => {
+	return (
+		<div
+			role="button"
+			aria-label="scroll to top"
+			tabIndex={0}
+			onClick={scrolltoTop}
+			className={` sticky top-[80%] w-fit ml-auto text-white   cursor-pointer`}
+		>
+			<div className="h-12 mr-5 w-12 grid active:scale-[1.03] ml-auto bg-grey-light/70 rounded-full justify-center items-center">
+				<NorthIcon fontSize="small" color="inherit" />
+			</div>
+		</div>
+	);
+};
+
+export default ChatScrolltoBottom;
