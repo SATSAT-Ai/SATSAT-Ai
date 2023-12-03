bb8ae12f22ae85a89f1ae94d3b7ff17bf06da0a3
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-03 02:23:33 +0000

    change outgoing chat icons

--- components/OutgoingMessage.tsx
+++ components/OutgoingMessage.tsx
@@ -4,10 +4,14 @@ import PersonPinIcon from "@mui/icons-material/PersonPin";
 
 const OutgoingMessage = ({ message }: { message: string }) => {
 	return (
-		<div className="flex items-end gap-3">
+		<div className="flex items-end gap-3 ml-auto">
 			<div className="outmessage flex shadow-lg flex-col items-end gap-3 p-4 rounded-3xl text-white">
 				<p>{message}</p>
-				<MdEditNote color="white" size={25} />
+				<MdEditNote
+					color="white"
+					className="active:scale-[1.03] cursor-pointer"
+					size={25}
+				/>
 			</div>
 			<PersonPinIcon fontSize="large" color="inherit" />
 		</div>
