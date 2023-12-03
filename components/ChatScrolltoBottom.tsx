8b3c50b375438204854f8089a85417af7e43fd57
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-03 02:15:06 +0000

    add scroll to bottom button in chats

---
+++ components/ChatScrolltoBottom.tsx
@@ -0,0 +1,27 @@
+import SouthIcon from "@mui/icons-material/South";
+
+const ChatScrolltoTop = ({
+	scrollToBottom,
+	isScrolledtoTop,
+}: {
+	scrollToBottom: () => void;
+	isScrolledtoTop: boolean;
+}) => {
+	return (
+		<div
+			tabIndex={0}
+			role="button"
+			aria-label="scroll to bottom"
+			onClick={scrollToBottom}
+			className={`${
+				!isScrolledtoTop && "!hidden"
+			} sticky top-[80%] translate-y-1/2 mx-auto  text-white active:scale-[1.03] h-14 w-14 grid aspect-square justify-center items-center rounded-full cursor-pointer`}
+		>
+			<div className="bg-grey-light/70 h-12 w-12 rounded-full grid items-center justify-center">
+				<SouthIcon fontSize="small" color="inherit" />
+			</div>
+		</div>
+	);
+};
+
+export default ChatScrolltoTop;
