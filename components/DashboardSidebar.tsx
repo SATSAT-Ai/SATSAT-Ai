1673b4927265fbfbfdc49db488337e3a86d49940
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-03 02:26:07 +0000

    update dashboard sidebar

--- components/DashboardSidebar.tsx
+++ components/DashboardSidebar.tsx
@@ -1,16 +1,25 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useContext } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { AppContext } from "@/context/AppContext";
 import DashboardSidebarWithData from "./DashboardSidebarData";
-
 const DashboardSidebar = () => {
 	const pathname = usePathname();
 	const { hideSidebar, setHideSidebar } = useContext(AppContext);
+
+	useLayoutEffect(() => {
+		if (pathname.includes("/chat")) {
+			setHideSidebar(true);
+		} else {
+			setHideSidebar(false);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [pathname]);
+
 	return (
 		<aside
-			className={`bg-white/10   backdrop-blur-md text-white  z-40 sm:relative fixed flex-initial md:basis-64  ${
+			className={`bg-white/10  backdrop-blur-md text-white  z-40 sm:relative fixed flex-initial md:basis-64  ${
 				hideSidebar ? "!basis-28 hidden md:block" : "block"
 			}`}
 		>
