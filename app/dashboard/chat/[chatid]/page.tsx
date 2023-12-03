6bab783a92503781508503c02d064a45124ce606
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-03 02:27:58 +0000

    update chat page

--- app/dashboard/chat/[chatid]/page.tsx
+++ app/dashboard/chat/[chatid]/page.tsx
@@ -4,12 +4,12 @@ import ToggleSidebars from "@/components/ToggleSidebars";
 const page = ({ params: { chatid } }: { params: { chatid: string } }) => {
 	return (
 		<>
-			<div className=" sticky overflow-y-auto  top-10 w-full my-min-h gap-5">
+			<div className=" sticky overflow-y-auto  top-10 w-full h-screen gap-5">
 				<div className="flex justify-between">
 					<div className="flex flex-1 w-full  flex-col">
 						<ToggleSidebars />
-						<div>
-							<h1 className="text-white">{chatid}</h1> main
+						<div className="p-5 mt-16">
+							<h1 className="text-white m-0">{chatid}</h1>
 						</div>
 					</div>
 					<ChatSidebar />
--- app/dashboard/chat/page.tsx
+++ app/dashboard/chat/page.tsx
@@ -3,43 +3,49 @@ import ToggleSidebars from "@/components/ToggleSidebars";
 import { Metadata } from "next";
 import TelegramIcon from "@mui/icons-material/Telegram";
 
+import ChatMain from "@/components/ChatMain";
+
 export const metadata: Metadata = {
 	title: "SATSAT-AI Chat with your financial data",
 };
 
 const page = () => {
 	return (
-		<div className="flex min-h-screen justify-between">
-			<div className="flex flex-[3] w-full flex-col">
+		<div className="flex h-full justify-between">
+			<div className="flex  flex-[3] h-screen overflow-hidden w-full flex-col">
 				<ToggleSidebars />
-				<div className=" flex flex-col h-full items-center justify-center px-3">
-					<h1 className="text-white text-text-40 text-center sm:text-text-60">
-						SATSAT AI
-					</h1>
-					<div className="text-white mt-10 max-w-xs md:max-w-full">
-						<h2 className="font-medium mx-auto w-fit xl:mr-auto xl:w-full">
-							Chat suggestions
-						</h2>
-						<div className="flex justify-center flex-wrap gap-5">
-							<div className="flex flex-col gap-5">
-								<button className="border border-white py-2 px-4 text-text-12 sm:text-text-normal hover:bg-brand-green/20 active:scale-[1.01] rounded-3xl">
-									Total income for the past quarter
-								</button>
-								<button className="border border-white py-2 px-4 text-text-12 sm:text-text-normal hover:bg-brand-green/20 active:scale-[1.01] rounded-3xl">
-									Total income for the past quarter
-								</button>
-							</div>
-							<div className="flex flex-col gap-5">
-								<button className="border border-white py-2 px-4 text-text-12 sm:text-text-normal hover:bg-brand-green/20 active:scale-[1.01] rounded-3xl">
-									Total income for the past quarter
-								</button>
-								<button className="border border-white py-2 px-4 text-text-12 sm:text-text-normal hover:bg-brand-green/20 active:scale-[1.01] rounded-3xl">
-									Total income for the past quarter
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
+				{
+					<ChatMain />
+
+					// <div className=" flex flex-col h-full items-center justify-center px-3">
+					// 	<h1 className="text-white text-text-40 text-center sm:text-text-60">
+					// 		SATSAT AI
+					// 	</h1>
+					// 	<div className="text-white mt-10 max-w-xs md:max-w-full">
+					// 		<h2 className="font-medium mx-auto w-fit xl:mr-auto xl:w-full">
+					// 			Chat suggestions
+					// 		</h2>
+					// 		<div className="flex justify-center flex-wrap gap-5">
+					// 			<div className="flex flex-col gap-5">
+					// 				<button type="button" className="border border-white py-2 px-4 text-text-12 sm:text-text-normal hover:bg-brand-green/20 active:scale-[1.01] rounded-3xl">
+					// 					Total income for the past quarter
+					// 				</button>
+					// 				<button type="button"className="border border-white py-2 px-4 text-text-12 sm:text-text-normal hover:bg-brand-green/20 active:scale-[1.01] rounded-3xl">
+					// 					Total income for the past quarter
+					// 				</button>
+					// 			</div>
+					// 			<div className="flex flex-col gap-5">
+					// 				<button type="button" className="border border-white py-2 px-4 text-text-12 sm:text-text-normal hover:bg-brand-green/20 active:scale-[1.01] rounded-3xl">
+					// 					Total income for the past quarter
+					// 				</button>
+					// 				<button type="button" className="border border-white py-2 px-4 text-text-12 sm:text-text-normal hover:bg-brand-green/20 active:scale-[1.01] rounded-3xl">
+					// 					Total income for the past quarter
+					// 				</button>
+					// 			</div>
+					// 		</div>
+					// 	</div>
+					// </div>
+				}
 				<div className="text-white sticky bottom-0 p-5  ">
 					<div className="bg-[#071f07] rounded-lg max-w-3xl mx-auto">
 						<div
@@ -57,6 +63,7 @@ const page = () => {
 								fontSize="large"
 								className="active:scale-[1.02]"
 								color="inherit"
+								aria-hidden="false"
 							/>
 						</div>
 					</div>
