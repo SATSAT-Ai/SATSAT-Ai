cde570f4212fe0f2c078ac2e05548a0da04bce97
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-10 01:40:13 +0000

    refactor: add a chatContext to handle logic in dashboard/chat page

---
+++ context/ChatContext.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+	Dispatch,
+	ReactNode,
+	SetStateAction,
+	createContext,
+	useState,
+} from "react";
+
+interface Ichat {
+	hideChatSidebar: boolean;
+	setHideChatSidebar: Dispatch<SetStateAction<boolean>>;
+}
+
+export const ChatContext = createContext<Ichat>({
+	hideChatSidebar: false,
+	setHideChatSidebar: () => {},
+});
+
+const ChatContextProvider = ({ children }: { children: ReactNode }) => {
+	const [hideChatSidebar, setHideChatSidebar] = useState(false);
+
+	return (
+		<ChatContext.Provider
+			value={{
+				hideChatSidebar,
+				setHideChatSidebar,
+			}}
+		>
+			{children}
+		</ChatContext.Provider>
+	);
+};
+
+export default ChatContextProvider;
