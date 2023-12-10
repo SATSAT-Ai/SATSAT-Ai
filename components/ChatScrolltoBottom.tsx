da943ce8165dafdfe05a99fbef8ba8bf551ec429
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-10 01:59:10 +0000

    refactor: improved scroll to top and bottom functionality

--- components/ChatScrolltoBottom.tsx
+++ components/ChatScrolltoBottom.tsx
@@ -2,10 +2,8 @@ import SouthIcon from "@mui/icons-material/South";
 
 const ChatScrolltoTop = ({
 	scrollToBottom,
-	isScrolledtoTop,
 }: {
 	scrollToBottom: () => void;
-	isScrolledtoTop: boolean;
 }) => {
 	return (
 		<div
@@ -13,9 +11,7 @@ const ChatScrolltoTop = ({
 			role="button"
 			aria-label="scroll to bottom"
 			onClick={scrollToBottom}
-			className={`${
-				!isScrolledtoTop && "!hidden"
-			} sticky top-[80%] translate-y-1/2 mx-auto  text-white active:scale-[1.03] h-14 w-14 grid aspect-square justify-center items-center rounded-full cursor-pointer`}
+			className={` sticky top-[80%] translate-y-1/2 mx-auto  text-white active:scale-[1.03] h-14 w-14 grid aspect-square justify-center items-center rounded-full cursor-pointer`}
 		>
 			<div className="bg-grey-light/70 h-12 w-12 rounded-full grid items-center justify-center">
 				<SouthIcon fontSize="small" color="inherit" />
--- components/ChatScrolltoTop.tsx
+++ components/ChatScrolltoTop.tsx
@@ -7,7 +7,7 @@ const ChatScrolltoBottom = ({ scrolltoTop }: { scrolltoTop: () => void }) => {
 			aria-label="scroll to top"
 			tabIndex={0}
 			onClick={scrolltoTop}
-			className={` sticky top-[80%] w-fit ml-auto text-white   cursor-pointer`}
+			className={` fixed left-2/3 top-[80%] w-fit ml-auto text-white   cursor-pointer`}
 		>
 			<div className="h-12 mr-5 w-12 grid active:scale-[1.03] ml-auto bg-grey-light/70 rounded-full justify-center items-center">
 				<NorthIcon fontSize="small" color="inherit" />
