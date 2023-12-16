b3a9c3d750467938d2cfe640dd37ccc38b23fdff
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-16 20:28:16 +0000

    refactor: removed unused styles

--- app/dashboard/chat/[chatid]/page.tsx
+++ app/dashboard/chat/[chatid]/page.tsx
@@ -5,7 +5,7 @@ import ToggleSidebars from "@/components/ToggleSidebars";
 const page = ({ params: { chatid } }: { params: { chatid: string } }) => {
 	return (
 		<>
-			<div className=" sticky overflow-y-auto  top-10 w-full h-screen gap-5">
+			<div className=" sticky overflow-y-auto overflow-x-clip  top-10 w-full h-screen gap-5">
 				<div className="flex justify-between">
 					<div className="flex flex-[3] h-screen overflow-hidden w-full flex-col">
 						<ToggleSidebars />
