9767fa81d4d3c577a969f5a052ba3f8c86a6de82
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-16 21:08:52 +0000

    feat: scroll to top will be removed once old conversation is continued

--- context/ChatContext.tsx
+++ context/ChatContext.tsx
@@ -10,22 +10,29 @@ import {
 
 interface Ichat {
 	hideChatSidebar: boolean;
+	isOldConversation: boolean;
 	setHideChatSidebar: Dispatch<SetStateAction<boolean>>;
+	setIsOldConversation: Dispatch<SetStateAction<boolean>>;
 }
 
 export const ChatContext = createContext<Ichat>({
 	hideChatSidebar: false,
+	isOldConversation: false,
 	setHideChatSidebar: () => {},
+	setIsOldConversation: () => {},
 });
 
 const ChatContextProvider = ({ children }: { children: ReactNode }) => {
 	const [hideChatSidebar, setHideChatSidebar] = useState(true);
+	const [isOldConversation, setIsOldConversation] = useState(false);
 
 	return (
 		<ChatContext.Provider
 			value={{
 				hideChatSidebar,
 				setHideChatSidebar,
+				setIsOldConversation,
+				isOldConversation,
 			}}
 		>
 			{children}
