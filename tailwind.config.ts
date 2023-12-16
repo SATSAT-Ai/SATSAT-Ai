67eee6de71698f51819026d557cec1084f74dfca
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-16 21:12:34 +0000

    refactor: added styles for shadcn date range picker

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,64 @@ const config: Config = {
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			backgroundImage: {
+				"silver-gradient": "linear-gradient(to top,#050d0a,#ffffff,#050d0a)",
+				"silver-gradient2": "linear-gradient(to left,#050d0a,#ffffff,#050d0a)",
+			},
+			colors: {
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsla(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
+		},
 		colors: {
 			"brand-green": "#29a173",
 			"brand-green-darker": "#174634",
@@ -17,13 +75,6 @@ const config: Config = {
 			"grey-light": "#444c48",
 			transparent: "transparent",
 			crimson: "crimson",
-		},
-
-		extend: {
-			backgroundImage: {
-				"silver-gradient": "linear-gradient(to top,#050d0a,#ffffff,#050d0a)",
-				"silver-gradient2": "linear-gradient(to left,#050d0a,#ffffff,#050d0a)",
-			},
 		},
 
 		fontSize: {
