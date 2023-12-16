c4232097265eb08394407e282cd76e2c8789338b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-16 21:20:35 +0000

    refactor: removed unsused styles from scroll to bottom

--- components/ChatScrolltoBottom.tsx
+++ components/ChatScrolltoBottom.tsx
@@ -6,17 +6,17 @@ const ChatScrolltoTop = ({
 	scrollToBottom: () => void;
 }) => {
 	return (
-		<div
+		<button
+			type="button"
 			tabIndex={0}
-			role="button"
 			aria-label="scroll to bottom"
 			onClick={scrollToBottom}
-			className={` sticky top-[80%] translate-y-1/2 mx-auto  text-white active:scale-[1.03] h-14 w-14 grid aspect-square justify-center items-center rounded-full cursor-pointer`}
+			className={`left-2/3 w-fit ml-auto text-white  cursor-pointer`}
 		>
 			<div className="bg-grey-light/70 h-12 w-12 rounded-full grid items-center justify-center">
 				<SouthIcon fontSize="small" color="inherit" />
 			</div>
-		</div>
+		</button>
 	);
 };
 
