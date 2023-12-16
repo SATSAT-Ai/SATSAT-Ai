90025673431b55ec942332164eb882a405310337
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-16 21:20:05 +0000

    refactor: removed unsused styles from scroll to top

--- components/ChatScrolltoTop.tsx
+++ components/ChatScrolltoTop.tsx
@@ -2,17 +2,17 @@ import NorthIcon from "@mui/icons-material/North";
 
 const ChatScrolltoBottom = ({ scrolltoTop }: { scrolltoTop: () => void }) => {
 	return (
-		<div
-			role="button"
+		<button
+			type="button"
 			aria-label="scroll to top"
 			tabIndex={0}
 			onClick={scrolltoTop}
-			className={` fixed left-2/3 top-[80%] w-fit ml-auto text-white   cursor-pointer`}
+			className={`  left-2/3 w-fit ml-auto text-white  cursor-pointer`}
 		>
-			<div className="h-12 mr-5 w-12 grid active:scale-[1.03] ml-auto bg-grey-light/70 rounded-full justify-center items-center">
+			<div className="h-12 w-12 grid active:scale-[1.03] ml-auto bg-grey-light/70 rounded-full justify-center items-center">
 				<NorthIcon fontSize="small" color="inherit" />
 			</div>
-		</div>
+		</button>
 	);
 };
 
