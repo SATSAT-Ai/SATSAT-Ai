14cc65f8ebccdcf14dbe453d8a05c5811e023dad
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-18 22:54:17 +0000

    refactor: add archive and delete chat icons

--- components/SingleChat.tsx
+++ components/SingleChat.tsx
@@ -3,8 +3,10 @@
 import { Ichat } from "@/interface";
 import Link from "next/link";
 import { PiChatCircleTextFill } from "react-icons/pi";
-import { SlOptionsVertical } from "react-icons/sl";
+import { SlOptions } from "react-icons/sl";
 import { useState, useRef, useEffect, Dispatch, SetStateAction } from "react";
+import { MdDelete } from "react-icons/md";
+import { BsArchiveFill } from "react-icons/bs";
 
 const SingleChat = ({
 	pathname,
@@ -17,11 +19,13 @@ const SingleChat = ({
 }) => {
 	const [showOptions, setShowOptions] = useState(false);
 
-	const optionRef = useRef<null | HTMLButtonElement>(null);
+	const optionRef = useRef<null | HTMLDivElement>(null);
 
 	useEffect(() => {
 		const handleClickOutSide = (event: any) => {
 			if (!optionRef.current?.contains(event.target)) {
+				return;
+			} else {
 				setShowOptions(false);
 			}
 		};
@@ -34,13 +38,18 @@ const SingleChat = ({
 
 	const handleDelete = (chatId: string) => {
 		//delete chat
+		setShowOptions(false);
+	};
+	const handleArchive = (chatId: string) => {
+		//archive chat
+		setShowOptions(false);
 	};
 
 	return (
 		<div
 			key={chat.id}
-			className={`flex relative items-center gap-0 px-1 hover:bg-white/10 ${
-				showOptions && "bg-white/10"
+			className={`flex relative items-center gap-0 px-2 hover:bg-white/10 ${
+				pathname.includes(chat.id) && "bg-white/10"
 			} rounded-xl`}
 		>
 			<Link
@@ -56,12 +65,8 @@ const SingleChat = ({
 				<PiChatCircleTextFill size={25} className="cursor-pointer" />
 				{chat.title}
 			</Link>
-			<button
-				onClick={() => setShowOptions((prev) => !prev)}
-				ref={optionRef}
-				type="button"
-			>
-				<SlOptionsVertical
+			<button onClick={() => setShowOptions((prev) => !prev)} type="button">
+				<SlOptions
 					size={17}
 					color="white"
 					className={` ${
@@ -70,13 +75,24 @@ const SingleChat = ({
 				/>
 			</button>
 			{showOptions && (
-				<div className="absolute z-10 right-3 top-10 bg-darker p-2 text-white text-[15px] rounded-lg">
+				<div
+					ref={optionRef}
+					className="absolute z-10 right-3 top-10 bg-darker p-2 text-white text-[15px] rounded-lg"
+				>
 					<ul className="flex items-center w-full text-[14px] font-medium flex-col gap-2 ">
 						<li
 							onClick={() => handleDelete(chat.id)}
-							className=" cursor-pointer active:scale-[1.02] text-crimson hover:text-white hover:bg-crimson rounded-md py-2 px-7"
+							className=" flex items-center gap-2 cursor-pointer active:scale-[1.02] text-crimson hover:text-white hover:bg-crimson rounded-md py-2 px-7"
 						>
-							Delete
+							<MdDelete size={20} />
+							Delete chat
+						</li>
+						<li
+							onClick={() => handleArchive(chat.id)}
+							className="flex items-center gap-3 cursor-pointer active:scale-[1.02] text-mid--yellow hover:bg-mid--yellow hover:text-white rounded-md py-2 px-7"
+						>
+							<BsArchiveFill size={19} />
+							Archive
 						</li>
 					</ul>
 				</div>
