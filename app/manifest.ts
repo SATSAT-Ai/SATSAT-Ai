634d9f3378a32387f305f2cdfe697b8073700343
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-12-25 12:40:46 +0000

    Fix: removed nested image path

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,16 +4,18 @@ import icon from "./icon.svg";
 export default function manifest(): MetadataRoute.Manifest {
 	return {
 		name: "SATSAT-AI",
-		short_name: "SATSAT-AI",
+		short_name: "SATSAT Ai",
 		description:
 			"SATSAT AI is an all-in-one artificial intelligence platform that combines cutting-edge AI technology to query your financial data with natural language.",
 		start_url: "/",
 		display: "standalone",
 		background_color: "#050d0a",
 		theme_color: "#fff",
+
 		icons: [
 			{
-				src: icon,
+				src: icon?.src,
+				purpose: "maskable" || "any",
 				sizes: "any",
 				type: "image/x-icon",
 			},
