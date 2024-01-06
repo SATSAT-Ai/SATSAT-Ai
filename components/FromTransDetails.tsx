0bb8cc3bf407c3890d15b00fd2d569cd589c0113
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-06 01:02:50 +0000

    refactor: add sender information to transaction details

---
+++ components/FromTransDetails.tsx
@@ -0,0 +1,34 @@
+import { Data } from "@/interface";
+
+const FromTransDetails = ({ transData }: { transData: Data[] }) => {
+	return (
+		<ul>
+			{Object.entries(transData).map(([key, value]) => {
+				return (
+					<div key={key} className="bg-[#2aa27438] rounded-3xl shadow-md p-4">
+						<div className="flex flex-col gap-5 justify-between pt-5">
+							{Object.entries(value).map(([keys, values]) => {
+								if (keys.startsWith("From")) {
+									return (
+										<div
+											key={values}
+											className="flex w-full items-center justify-between gap-5"
+										>
+											<p className="capitalize text-[#ffffffd3] ">
+												{keys.split(" ")[1]}
+											</p>
+											<p className="capitalize">{values}</p>
+										</div>
+									);
+								}
+								return;
+							})}
+						</div>
+					</div>
+				);
+			})}
+		</ul>
+	);
+};
+
+export default FromTransDetails;
