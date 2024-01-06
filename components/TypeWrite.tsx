c49201cd6d31de139a736f17371466f149750260
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-06 01:50:12 +0000

    refactor: typeWriter is reusable and added more options

--- components/TypeWrite.tsx
+++ components/TypeWrite.tsx
@@ -1,31 +1,93 @@
 "use client";
+import { useState, useEffect, Dispatch, SetStateAction, memo } from "react";
 
-import { useState, useEffect } from "react";
+interface ITypeWrite {
+	text: string | string[];
+	color?: string;
+	showCaret?: boolean;
+	interval?: number;
+	setTypeWriterComplete?: Dispatch<SetStateAction<boolean>>;
+	loop?: boolean;
+	timeToStartNewText?: number;
+}
 
-const TypeWrite = ({ text }: { text: string }) => {
+const TypeWrite = ({
+	text,
+	color,
+	showCaret,
+	interval = 60,
+	setTypeWriterComplete,
+	loop = false,
+	timeToStartNewText = 2000,
+}: ITypeWrite) => {
 	const [typedText, setTypedText] = useState("");
 	const [cursorVisible, setCursorVisible] = useState(true);
+	const [currentWordIndex, setCurrentWordIndex] = useState(0);
 
 	useEffect(() => {
-		const typingInterval = setInterval(() => {
-			if (typedText !== text) {
-				setTypedText(typedText + text[typedText.length]);
-			} else {
-				clearInterval(typingInterval);
-				setCursorVisible(false);
+		if (setTypeWriterComplete) {
+			setTypeWriterComplete(typedText === text);
+		}
+	}, [currentWordIndex, setTypeWriterComplete, text, typedText]);
+
+	useEffect(() => {
+		const sentence = Array.isArray(text) ? text : [text];
+
+		const simulateTypewriterEffect = (textToType: string) => {
+			const intervalId = setInterval(() => {
+				setTypedText((prevTypedText) => {
+					const nextChar = textToType && textToType[prevTypedText.length];
+					if (nextChar) {
+						return prevTypedText + nextChar;
+					} else {
+						setCurrentWordIndex(currentWordIndex + 1);
+						clearInterval(intervalId);
+					}
+
+					return prevTypedText;
+				});
+			}, interval);
+		};
+
+		let timeOutId: string | number | NodeJS.Timeout | undefined;
+
+		if (currentWordIndex < sentence.length) {
+			timeOutId = setTimeout(() => {
+				setTypedText("");
+				setCursorVisible(true);
+				simulateTypewriterEffect(sentence[currentWordIndex]);
+			}, timeToStartNewText); //when to start the new text
+		} else {
+			if (loop) {
+				timeOutId = setTimeout(() => {
+					setCursorVisible(true);
+					setCurrentWordIndex(0);
+				}, interval); //when to loop the new text
 			}
-		}, 50);
+		}
 
 		return () => {
-			clearInterval(typingInterval);
+			clearTimeout(timeOutId);
 		};
-	}, [text, typedText]);
+	}, [currentWordIndex, interval, loop, text, timeToStartNewText]);
 
 	return (
-		<div className="flex items-center justify-center flex-wrap text-mid--yellow text-text-normal md:text-text-normal">
+		<div
+			className={`flex items-center flex-wrap ${
+				color ? `text-[${color}]` : "text-mid--yellow"
+			} text-text-normal md:text-text-20`}
+		>
 			<span>{typedText}</span>
-			{cursorVisible && (
-				<span className={cursorVisible ? "animate-pulse text-mid--yellow" : ""}>
+			{cursorVisible && showCaret && (
+				<span
+					className={`${
+						cursorVisible
+							? `animate-pulse ${
+									color ? `text-[${color}]` : "text-mid--yellow"
+							  }`
+							: ""
+					}`}
+				>
 					|
 				</span>
 			)}
@@ -33,4 +95,4 @@ const TypeWrite = ({ text }: { text: string }) => {
 	);
 };
 
-export default TypeWrite;
+export default memo(TypeWrite);
