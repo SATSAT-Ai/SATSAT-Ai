8c7f85f08eb91b525f6ab0984d7f691683d34742
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-06 02:23:21 +0000

    feat: add category details page

--- app/dashboard/transactions/categories/[categoryId]/page.tsx
+++ app/dashboard/transactions/categories/[categoryId]/page.tsx
@@ -1,5 +1,4 @@
-import TransactionDetails from "@/components/TransactionDetailsPage";
-import { Data } from "@/interface";
+import CategoryDetails from "@/components/CategoryDetails";
 
 const page = ({
 	params: { categoryId },
@@ -8,166 +7,91 @@ const page = ({
 }) => {
 	// fetch data based on paramId
 
-	const transData: Data[] = [
+	const categoryData = [
 		{
-			id: "sdjfksdjf",
-			reconcilable: true,
-			Provider: "MTN",
-			"Trans. Date": "02-20-2023",
-			Category: "ECG",
-			"From Acc.": 342343,
-			"From Name": "Kamasah",
-			"From No.": 231312323,
-			"Trans. Type": "Transfer",
-			Amount: 20,
-			Fees: 0.32,
-			E_Levy: 0.2,
-			"Bal. Before": 64.5,
-			"Bal. After": 30.5,
-			"To No.": 243743334,
-			"To Name": "Felicia",
-			"To Acc.": 4234342,
-			"Trans. ID": "234f435f",
-			"Ref.": "Ice cream",
-			Ova: "iriej343",
-			"Img.": "9jijfjkjjk",
-			Details: "Details",
+			id: "sdf9sf3434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
 		},
 		{
-			id: "sdjfksdjdf34f",
-			reconcilable: true,
-			Provider: "Vodafone",
-			"Trans. Date": "22-20-2023",
-			Category: "Water",
-			"From Acc.": 3423343,
-			"From Name": "Felicia",
-			"From No.": 23131522323,
-			"Trans. Type": "Transfer",
-			Amount: 230,
-			Fees: 0.323,
-			E_Levy: 0.22,
-			"Bal. Before": 634.5,
-			"Bal. After": 320.5,
-			"To No.": 2433743334,
-			"To Name": "Dickson",
-			"To Acc.": 42343342,
-			"Trans. ID": "232fg4f435f",
-			"Ref.": "Ice cream",
-			Ova: "irie34j343",
-			"Img.": "9jijf6fjkjjk",
-			Details: "Details",
+			id: "sdf93sdf434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
 		},
 		{
-			id: "sdjfksdj453df34f",
-			Provider: "Tigo",
-			reconcilable: false,
-			"Trans. Date": "22-21-2023",
-			Category: "Airtime",
-			"From Acc.": 342334343,
-			"From Name": "Edith",
-			"From No.": 231315223323,
-			"Trans. Type": "Transfer",
-			Amount: 230,
-			Fees: 0.3,
-			E_Levy: 0.2,
-			"Bal. Before": 34.5,
-			"Bal. After": 30.5,
-			"To No.": 24337243334,
-			"To Name": "Dickson",
-			"To Acc.": 4234563342,
-			"Trans. ID": "232f23g4f435f",
-			"Ref.": "Ice cream",
-			Ova: "irie3e4j343",
-			"Img.": "9jijf6f4fjkjjk",
-			Details: "Details",
+			id: "sdf9334434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
+		},
+		{
+			id: "sdf9ff3434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
+		},
+		{
+			id: "sdfsd93434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
+		},
+		{
+			id: "sdfsd93434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
+		},
+		{
+			id: "sdfsd93434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
+		},
+		{
+			id: "sdfsd93434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
+		},
+		{
+			id: "sdfsd93434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
+		},
+		{
+			id: "sdfsd93434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
+		},
+		{
+			id: "sdfsd93434",
+			date: "12-01-2023",
+			name: "Frozen yoghurt",
+			number: 8,
+			Amount: 24,
 		},
 	];
 
-	// const headCells: GridColDef[] = [
-	// 	{
-	// 		field: "Provider",
-	// 	},
-	// 	{
-	// 		field: "Trans. Date",
-	// 	},
-	// 	{
-	// 		field: "Category",
-	// 	},
-	// 	{
-	// 		field: "From Acc.",
-	// 	},
-	// 	{
-	// 		field: "From Name",
-	// 	},
-	// 	{
-	// 		field: "From No.",
-	// 	},
-	// 	{
-	// 		field: "Trans. Type",
-	// 	},
-	// 	{
-	// 		field: "Amount",
-	// 	},
-	// 	{
-	// 		field: "Fees",
-	// 	},
-	// 	{
-	// 		field: "E_Levy",
-	// 	},
-	// 	{
-	// 		field: "Bal. Before",
-	// 	},
-	// 	{
-	// 		field: "Bal. After",
-	// 	},
-	// 	{
-	// 		field: "To No.",
-	// 	},
-	// 	{
-	// 		field: "To Name",
-	// 	},
-	// 	{
-	// 		field: "To Acc.",
-	// 	},
-	// 	{
-	// 		field: "Trans. ID",
-	// 	},
-	// 	{
-	// 		field: "Ref.",
-	// 	},
-	// 	{
-	// 		field: "Ova",
-	// 	},
-	// 	{
-	// 		field: "Img.",
-	// 		renderCell: (params) => {
-	// 			const imageSrc = getProviderImage(params.row.Provider);
-	// 			return (
-	// 				<Image
-	// 					src={imageSrc}
-	// 					alt="Provider logo"
-	// 					style={{ width: "35px", height: "35px", borderRadius: "100%" }}
-	// 				/>
-	// 			);
-	// 		},
-	// 	},
-	// 	{
-	// 		field: "Details",
+	const filteredcategoryData = categoryData.filter(
+		(data) => data.id === categoryId
+	);
 
-	// 		renderCell: (params) => {
-	// 			return (
-	// 				<Link
-	// 					className="py-2 px-3 hover:bg-brand-green text-text-12 text-white bg-darker shadow-md active:scale-[1.02] rounded-lg "
-	// 					href={`/dashboard/transactions/${params.id}`}
-	// 				>
-	// 					Details
-	// 				</Link>
-	// 			);
-	// 		},
-	// 	},
-	// ];
-
-	return <TransactionDetails transData={transData} />;
+	return <CategoryDetails categoryData={categoryData} />;
 };
 
 export default page;
