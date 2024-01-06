2e72a22a77d84caee1d8175703e5663146f25db2
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-06 02:23:46 +0000

    feat: add category details page

---
+++ components/CategoryDetails.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CategoryDetails = ({
+	categoryData,
+}: {
+	categoryData: {
+		id: string;
+		date: string;
+		name: string;
+		number: number;
+		Amount: number;
+	}[];
+}) => {
+	return <div>CategoryDetails</div>;
+};
+
+export default CategoryDetails;
