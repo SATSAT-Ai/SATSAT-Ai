8a47ae8789acf1bc5d87261ca3bbf1a2e685429d
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-09 03:47:58 +0000

    refactor: added animated gradient to pageScroller

--- components/PageScroller.tsx
+++ components/PageScroller.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { BsArrowUpCircleFill } from "react-icons/bs";
+import { TbSquareRoundedArrowUpFilled } from "react-icons/tb";
 
 const PageScroller = () => {
 	const [scrolled, setScrolled] = useState(false);
@@ -28,16 +28,16 @@ const PageScroller = () => {
 		};
 	}, []);
 	return (
-		<div
-			onClick={scrollTO}
-			className={` ${
-				scrolled ? "block" : "hidden"
-			} fixed bottom-20 z-10 cursor-pointer right-7`}
-		>
-			<BsArrowUpCircleFill
-				size={40}
-				className="text-white hover:text-mid--yellow duration-150 active:scale-[1.03]"
-			/>
+		<div className="fixed bottom-20 z-10 cursor-pointer right-7">
+			<button
+				onClick={scrollTO}
+				type="button"
+				className={`${
+					scrolled ? "block" : "hidden"
+				} before:opacity-0 hover:before:opacity-100 before:z-[-1] after:z-[-1]  before:rounded-3xl after:absolute after:rounded-3xl after:top-[-1px] after:left-[-1px]  before:absolute before:top-[-1px] before:left-[-1px] bg-transparent relative rounded-3xl bg-gradient-to-tr from-[#050e0b] to-[#000000] justify-between custom-block glow2 text-text-normal text-white font-medium flex items-center gap-2`}
+			>
+				<TbSquareRoundedArrowUpFilled size={45} color="white" className="p-1" />
+			</button>
 		</div>
 	);
 };
