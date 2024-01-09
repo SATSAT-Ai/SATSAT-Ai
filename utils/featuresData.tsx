eba39293a8e0e8f287db78a52305b1d72cefaa0f
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-09 03:52:35 +0000

    refactor: moved featuresData into utils

---
+++ utils/featuresData.tsx
@@ -0,0 +1,85 @@
+import { BsFillFileEarmarkBreakFill, BsRobot } from "react-icons/bs";
+import { MdDashboard, MdDocumentScanner } from "react-icons/md";
+import { PiDetectiveFill, PiWebhooksLogoBold } from "react-icons/pi";
+import { GiArtificialIntelligence } from "react-icons/gi";
+import { HiDocumentSearch } from "react-icons/hi";
+import { FaChartSimple } from "react-icons/fa6";
+import { featureData } from "../interface/interface";
+import { MdManageAccounts } from "react-icons/md";
+import { MdOutlineQueryStats } from "react-icons/md";
+import { IoDocumentLockOutline } from "react-icons/io5";
+
+export const featuresData: featureData[] = [
+	{
+		icon: <BsFillFileEarmarkBreakFill size={40} color="#c18e3b" />,
+		title: "Query OCR, PDF, CSV and More",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <MdDashboard size={40} color="#c18e3b" />,
+		title: "Intuitive Dashboard",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <PiDetectiveFill size={40} color="#c18e3b" />,
+		title: "Fraud Detection",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <BsRobot size={40} color="#c18e3b" />,
+		title: "AI chatbot",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <IoDocumentLockOutline size={40} color="#c18e3b" />,
+		title: "Smart Document Detection",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <MdDocumentScanner size={40} color="#c18e3b" />,
+		title: "Document Scanner",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <GiArtificialIntelligence size={40} color="#c18e3b" />,
+		title: "Artificial Intelligence and Machine Learning",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <HiDocumentSearch size={40} color="#c18e3b" />,
+		title: "Insight to financial documents",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <FaChartSimple size={40} color="#c18e3b" />,
+		title: "Charts and Graphs",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <MdManageAccounts size={40} color="#c18e3b" />,
+		title: "Client Profile Management",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <MdOutlineQueryStats size={40} color="#c18e3b" />,
+		title: "Alternative Scoring",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+	{
+		icon: <PiWebhooksLogoBold size={40} color="#c18e3b" />,
+		title: "API and Webhooks",
+		para: " amet amet pellentesque. Arcu mollis sem aenean aliquam diam enim tortor et. Pharetra neque interdum ac arcu nec morbi placerat in pellentesque. Eget ut adipiscing in ac. Turpis nunc sit in erat tristique facilisi vitae nec morbi.",
+		link: "#",
+	},
+];
