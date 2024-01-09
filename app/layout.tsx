9f4dada16d7f916a41705ddfc52377cf0d65836d
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-09 04:03:51 +0000

    Fix: compilation errors due to next/auth sessions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,25 @@
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.scss";
 import { SATSATmetadata } from "@/utils/metadata";
 import NextTopLoader from "nextjs-toploader";
 import "react-tooltip/dist/react-tooltip.css";
-import { Toaster } from "react-hot-toast";
-import { getServerSession } from "next-auth";
-import SessionProvider from "@/components/SessionProvider";
-import { options } from "./api/auth/[...nextauth]/options";
 
-const montserrat = Montserrat({
+const poppins = Poppins({
 	subsets: ["latin"],
-	weight: ["200", "300", "400", "500", "600", "700", "900"],
-	// roboto weight: ["300", "400", "500", "700", "800", "900"],
+	weight: ["300", "400", "500", "700", "800", "900"],
 });
 
 export const metadata: Metadata = SATSATmetadata;
 
-export default async function RootLayout({
+export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const session = await getServerSession(options);
 	return (
 		<html lang="en">
-			<body className={montserrat.className}>
+			<body className={poppins.className}>
 				<NextTopLoader
 					showSpinner={false}
 					color="#29a173"
@@ -37,8 +31,7 @@ export default async function RootLayout({
 					speed={350}
 					shadow="0 0 10px #29a173,0 0 5px #29a173"
 				/>
-				<Toaster />
-				<SessionProvider session={session}>{children}</SessionProvider>
+				{children}
 			</body>
 		</html>
 	);
