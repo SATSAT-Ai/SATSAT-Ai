48473f946918542cd98954474eacfc4f6e8cf007
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-09 03:59:00 +0000

    refactor:change import path for notification interface

--- components/NotificationLogic.tsx
+++ components/NotificationLogic.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import NotificationIcon from "@/public/notificationIcon.svg";
-import { Inotification } from "@/interface";
+import { Inotification } from "@/interface/interface";
 import { Dispatch, SetStateAction } from "react";
 
 const NotificationLogic = ({
