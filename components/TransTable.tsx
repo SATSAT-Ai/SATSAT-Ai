0cc64c60fffcfd709d39ac41c053d6b2fdc2a1c9
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-10 04:25:47 +0000

    refactor: change provider import path

--- components/TransTable.tsx
+++ components/TransTable.tsx
@@ -10,7 +10,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Data } from "@/interface/interface";
 import { useState } from "react";
-import { getProviderImage } from "@/utils/providerImages";
+import { getProviderImage } from "@/providerImages";
 
 export default function TransTable() {
 	const rows: Data[] = [
