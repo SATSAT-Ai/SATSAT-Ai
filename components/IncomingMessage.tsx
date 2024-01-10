5268ce118488ba18ef3d140cde1e967f6876d84b
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-10 04:14:56 +0000

    refactor: add more properties to incomingMessages

--- components/IncomingMessage.tsx
+++ components/IncomingMessage.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import TypeWrite from "./TypeWrite";
 
 // //track history of conversations
 // interface ParentChat {
@@ -13,6 +14,8 @@ import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
 // }
 
 interface IcomingProp {
+	fontSize?: number;
+	typeWrite?: boolean;
 	message: {
 		id: string;
 		firstText: string;
@@ -22,40 +25,72 @@ interface IcomingProp {
 	};
 }
 
-const IncomingMessage = ({ message }: IcomingProp) => {
+const IncomingMessage = ({ message, fontSize, typeWrite }: IcomingProp) => {
 	return (
-		<div className="flex items-end mr-auto gap-3">
-			<Image src={chatbot} height={35} width={35} alt="satsat-ai" />
-			<div className=" flex inMessage shadow-lg flex-col gap-3 p-4 rounded-3xl text-white">
-				{message.firstText && (
-					<div>
-						<p>{message.firstText}</p>
-					</div>
-				)}
+		<>
+			{typeWrite ? (
+				<div className="flex items-end mr-auto gap-3">
+					<Image src={chatbot} height={35} width={35} alt="satsat-ai" />
+					<div className=" flex inMessage w-full shadow-lg flex-col gap-1 p-4 rounded-3xl text-white">
+						{/* {message.firstText && (
+							<p
+								style={{
+									fontSize: `${fontSize ? fontSize : 16}px`,
+								}}
+							>
+								{message.firstText}
+							</p>
+						)} */}
+						<TypeWrite
+							text={message.firstText}
+							fontSize={fontSize}
+							color="white"
+							showCaret={true}
+							timeToStartNewText={1000}
+							typingSpeed={20}
+						/>
 
-				{message.list && (
-					<ul className="unordered-list">
-						{message.list.map((listMessage) => {
-							return (
-								<li className="list-disc list-inside" key={listMessage.id}>
-									{listMessage.msg}
-								</li>
-							);
-						})}
-					</ul>
-				)}
-				{message.endingText && (
-					<div>
-						<p>{message.endingText}</p>
-					</div>
-				)}
-				<div className="flex items-center gap-3 mt-3">
-					<ContentCopyIcon
-						fontSize="small"
-						className="active:scale-[1.03] cursor-pointer"
-						color="inherit"
-					/>
-					<ThumbUpAltIcon
+						{message.list && (
+							<ul className="unordered-list">
+								{message.list.map((listMessage) => {
+									return (
+										<li
+											style={{
+												fontSize: `${fontSize ? fontSize : 16}px`,
+											}}
+											className="list-disc list-inside"
+											key={listMessage.id}
+										>
+											<TypeWrite
+												text={listMessage.msg}
+												fontSize={fontSize}
+												color="white"
+												showCaret={true}
+												timeToStartNewText={1000}
+												typingSpeed={20}
+											/>
+										</li>
+									);
+								})}
+							</ul>
+						)}
+						{message.endingText && (
+							<TypeWrite
+								text={message.endingText}
+								fontSize={fontSize}
+								color="white"
+								showCaret={true}
+								timeToStartNewText={1000}
+								typingSpeed={20}
+							/>
+						)}
+						<div className="flex items-center gap-3 mt-3">
+							<ContentCopyIcon
+								fontSize="small"
+								className="active:scale-[1.03] cursor-pointer"
+								color="inherit"
+							/>
+							{/* <ThumbUpAltIcon
 						fontSize="small"
 						className="active:scale-[1.03] cursor-pointer"
 						color="inherit"
@@ -64,10 +99,71 @@ const IncomingMessage = ({ message }: IcomingProp) => {
 						fontSize="small"
 						className="active:scale-[1.03] cursor-pointer"
 						color="inherit"
-					/>
+					/> */}
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			) : (
+				<div className="flex w-full items-end mr-auto gap-3">
+					<Image src={chatbot} height={35} width={35} alt="satsat-ai" />
+					<div className=" flex inMessage shadow-lg flex-col gap-1 p-4 rounded-3xl text-white">
+						{message.firstText && (
+							<p
+								style={{
+									fontSize: `${fontSize ? fontSize : 16}px`,
+								}}
+							>
+								{message.firstText}
+							</p>
+						)}
+
+						{message.list && (
+							<ul className="unordered-list">
+								{message.list.map((listMessage) => {
+									return (
+										<li
+											style={{
+												fontSize: `${fontSize ? fontSize : 16}px`,
+											}}
+											className="list-disc list-inside"
+											key={listMessage.id}
+										>
+											{listMessage.msg}
+										</li>
+									);
+								})}
+							</ul>
+						)}
+						{message.endingText && (
+							<p
+								style={{
+									fontSize: `${fontSize ? fontSize : 16}px`,
+								}}
+							>
+								{message.endingText}
+							</p>
+						)}
+						<div className="flex items-center gap-3 mt-3">
+							<ContentCopyIcon
+								fontSize="small"
+								className="active:scale-[1.03] cursor-pointer"
+								color="inherit"
+							/>
+							{/* <ThumbUpAltIcon
+						fontSize="small"
+						className="active:scale-[1.03] cursor-pointer"
+						color="inherit"
+					/>
+					<ThumbDownIcon
+						fontSize="small"
+						className="active:scale-[1.03] cursor-pointer"
+						color="inherit"
+					/> */}
+						</div>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
