af4dd5e52c51b7a678554987fb7ab73c7301b1c4
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-10 04:14:25 +0000

    refactor:handle session and signIn on mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,9 @@
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { MdClose } from "react-icons/md";
-import { SetStateAction, Dispatch } from "react";
-
+import { SetStateAction, Dispatch, useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 interface navProps {
 	setShowNav: Dispatch<SetStateAction<boolean>>;
 	showNav: boolean;
@@ -10,6 +11,19 @@ interface navProps {
 
 const MobileNav = ({ setShowNav, showNav }: navProps) => {
 	const pathname = usePathname();
+	const { data: session } = useSession();
+	const [loading, setLoading] = useState(false);
+
+	const handleSignOut = async () => {
+		setLoading(true);
+		try {
+			await signOut();
+			setShowNav(false);
+		} catch (e) {
+			console.log(e);
+			setLoading(false);
+		}
+	};
 
 	return (
 		<>
@@ -40,7 +54,7 @@ const MobileNav = ({ setShowNav, showNav }: navProps) => {
 					className={`${
 						pathname == "/"
 							? "text-mid--yellow"
-							: "text-grey-lightest hover:text-white"
+							: "text-white hover:text-mid--yellow font-medium"
 					}`}
 				>
 					<Link href={"/"}>Home</Link>
@@ -51,7 +65,7 @@ const MobileNav = ({ setShowNav, showNav }: navProps) => {
 					className={`${
 						pathname == "/about"
 							? "text-mid--yellow"
-							: "text-grey-lightest hover:text-white"
+							: "text-white hover:text-mid--yellow font-medium"
 					}`}
 				>
 					<Link href={"/about"}>About</Link>
@@ -60,7 +74,7 @@ const MobileNav = ({ setShowNav, showNav }: navProps) => {
 					className={`${
 						pathname == "/how-it-works"
 							? "text-mid--yellow font-medium"
-							: "text-grey-lightest hover:text-white"
+							: "text-white hover:text-mid--yellow font-medium"
 					}`}
 				>
 					<Link href={"/how-it-works"}>How It Works</Link>
@@ -70,27 +84,59 @@ const MobileNav = ({ setShowNav, showNav }: navProps) => {
 					className={`${
 						pathname == "/features"
 							? "text-mid--yellow"
-							: "text-grey-lightest hover:text-white"
+							: "text-white hover:text-mid--yellow font-medium"
 					}`}
 				>
 					<Link href={"/features"}>Features</Link>
 				</li>
 
-				<li
-					onClick={() => setShowNav(false)}
-					className="text-grey-lightest hover:text-white"
-				>
-					<Link href={"/signin"}>Sign in</Link>
-				</li>
-				{pathname !== "/choose-your-pricing" && (
+				{!session?.user ? (
+					<li
+						onClick={() => setShowNav(false)}
+						className="text-white hover:text-mid--yellow font-medium"
+					>
+						<Link href={"/signin"}>Sign in</Link>
+					</li>
+				) : (
+					<li
+						onClick={() => setShowNav(false)}
+						className="text-white hover:text-mid--yellow font-medium"
+					>
+						<Link href={"/dashboard"}>Dashboard</Link>
+					</li>
+				)}
+				{pathname !== "/choose-your-pricing" && !session?.user ? (
+					// <li onClick={() => setShowNav(false)}>
+					// 	<Link
+					// 		className="flex items-center !rounded-3xl gap-3 font-normal hover:shadow-none hover:bg-mid--yellow transition-colors duration-200 active:scale-[1.01] text-white bg-brand-green button2"
+					// 		href={"/choose-your-pricing"}
+					// 	>
+					// 		Get Started
+					// 	</Link>
+					// </li>
 					<li onClick={() => setShowNav(false)}>
 						<Link
-							className="flex items-center !rounded-3xl gap-3 font-normal hover:shadow-none hover:bg-mid--yellow transition-colors duration-200 active:scale-[1.01] text-white bg-brand-green button2"
+							className={`px-7 before:opacity-0 hover:before:opacity-100 before:z-[-1] after:z-[-1]  before:rounded-3xl after:absolute after:rounded-3xl after:top-[-1px] after:left-[-1px]  before:absolute before:top-[-1px] before:left-[-1px] bg-transparent relative rounded-3xl bg-gradient-to-tr from-[#050e0b] to-[#000000] justify-between py-3 custom-block glow4 text-text-normal text-white font-medium flex items-center gap-2`}
 							href={"/choose-your-pricing"}
 						>
-							Get Started
+							Get Started Now
 						</Link>
 					</li>
+				) : (
+					pathname !== "/choose-your-pricing" &&
+					session?.user && (
+						<li className="flex items-center !rounded-3xl font-medium gap-3 hover:shadow-none hover:bg-mid--yellow transition-colors duration-200 active:scale-[1.01] text-white bg-brand-green button2">
+							<button
+								disabled={loading}
+								type="button"
+								onClick={handleSignOut}
+								className="flex items-center gap-4"
+							>
+								{loading ? "Signing out" : "Sign Out"}
+								{loading && <div className="loader"></div>}
+							</button>
+						</li>
+					)
 				)}
 			</ul>
 		</>
