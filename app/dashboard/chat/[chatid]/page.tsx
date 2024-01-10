b98a2517aa86bce988e896aecf46acfcbe40147d
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-10 04:04:12 +0000

    refactor: rename params chat id

--- app/dashboard/chat/[chatid]/page.tsx
+++ app/dashboard/chat/[chatid]/page.tsx
@@ -2,14 +2,14 @@ import ChatMain from "@/components/ChatMain";
 import ChatSidebar from "@/components/ChatSidebar";
 import ToggleSidebars from "@/components/ToggleSidebars";
 
-const page = ({ params: { chatid } }: { params: { chatid: string } }) => {
+const page = ({ params: { chatId } }: { params: { chatId: string } }) => {
 	return (
 		<>
 			<div className=" sticky overflow-y-auto overflow-x-clip  top-10 w-full h-screen gap-5">
 				<div className="flex justify-between">
 					<div className="flex flex-[3] h-screen overflow-hidden w-full flex-col">
 						<ToggleSidebars />
-						{<ChatMain chatContainerId={chatid} />}
+						{<ChatMain chatContainerId={chatId} />}
 					</div>
 					<ChatSidebar />
 				</div>
