d37045b3af75b8e82b29e3748933c1dd51bf4ed4
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-12 04:37:02 +0000

    refactor: showChatmain

--- app/dashboard/chat/page.tsx
+++ app/dashboard/chat/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
 			<div className="flex h-full overflow-clip justify-between">
 				<div className="flex  flex-[3] h-screen overflow-hidden w-full flex-col">
 					<ToggleSidebars />
-					{/* <ChatMain /> */}
+					<ChatMain />
 				</div>
 				<ChatSidebar />
 			</div>
