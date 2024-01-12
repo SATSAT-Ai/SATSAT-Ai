f5248e232ddf4a05016be8dc0dc6454b23755bdf
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-12 04:55:37 +0000

    refactor: change chat/chatId layout to /chat layout

--- app/dashboard/chat/[chatid]/page.tsx
+++ app/dashboard/chat/[chatid]/page.tsx
@@ -5,14 +5,12 @@ import ToggleSidebars from "@/components/ToggleSidebars";
 const page = ({ params: { chatId } }: { params: { chatId: string } }) => {
 	return (
 		<>
-			<div className=" sticky overflow-y-auto overflow-x-clip  top-10 w-full h-screen gap-5">
-				<div className="flex justify-between">
-					<div className="flex flex-[3] h-screen overflow-hidden w-full flex-col">
-						<ToggleSidebars />
-						{<ChatMain chatContainerId={chatId} />}
-					</div>
-					<ChatSidebar />
+			<div className="flex h-full overflow-clip justify-between">
+				<div className="flex flex-[3] h-full overflow-hidden w-full flex-col">
+					<ToggleSidebars />
+					<ChatMain chatContainerId={chatId} />
 				</div>
+				<ChatSidebar />
 			</div>
 		</>
 	);
