7f552e2edb90fb95be4ba62d1d3d436efbd057d9
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-12 03:07:15 +0000

    refactor:setHideschatSidebar to false

--- context/ChatContext.tsx
+++ context/ChatContext.tsx
@@ -28,7 +28,7 @@ export const ChatContext = createContext<Ichat>({
 });
 
 const ChatContextProvider = ({ children }: { children: ReactNode }) => {
-	const [hideChatSidebar, setHideChatSidebar] = useState(true);
+	const [hideChatSidebar, setHideChatSidebar] = useState(false);
 	const [isOldConversation, setIsOldConversation] = useState(false);
 	const [conversations, setConversations] = useState<IUser[]>([]);
 
