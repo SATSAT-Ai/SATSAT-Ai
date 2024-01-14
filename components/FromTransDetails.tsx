e7b5b8f6cc49290235f205117af1822c4c473c42
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-14 01:54:39 +0000

    style:change background of fromTransactions details

--- components/FromTransDetails.tsx
+++ components/FromTransDetails.tsx
@@ -5,14 +5,17 @@ const FromTransDetails = ({ transData }: { transData: Data[] }) => {
 		<ul>
 			{Object.entries(transData).map(([key, value]) => {
 				return (
-					<div key={key} className="bg-[#2aa27438] rounded-3xl shadow-md p-4">
+					<div
+						key={key}
+						className="border bg-[#1ece8b23]  border-[#2aa2747a] rounded-3xl shadow-md p-4"
+					>
 						<div className="flex flex-col gap-5 justify-between pt-5">
 							{Object.entries(value).map(([keys, values]) => {
 								if (keys.startsWith("From")) {
 									return (
 										<div
 											key={values}
-											className="flex w-full items-center justify-between gap-5"
+											className="flex w-full font-medium items-center justify-between gap-5"
 										>
 											<p className="capitalize text-[#ffffffd3] ">
 												{keys.split(" ")[1]}
