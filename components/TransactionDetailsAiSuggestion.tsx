c93cae7ae2d6992f0da4f8e21b3f06b879a245dd
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-14 01:59:14 +0000

    refactor:change Ui for ai suggestion

--- components/TransactionDetailsAiSuggestion.tsx
+++ components/TransactionDetailsAiSuggestion.tsx
@@ -20,6 +20,7 @@ interface ITransactionDetailsSuggestion {
 	detailsLength: number;
 	showMore: number;
 	loading: boolean;
+	list_suggestions: Isuggestions[];
 }
 
 const TransactionDetailsAiSuggestion = ({
@@ -27,92 +28,74 @@ const TransactionDetailsAiSuggestion = ({
 	detailsLength,
 	showMore,
 	loading,
+	list_suggestions,
 }: ITransactionDetailsSuggestion) => {
 	const [isTypeWriterComplete, setIsTypeWriterComplete] =
 		useState<boolean>(false);
 
-	const list_suggestions: Isuggestions[] = [
-		{
-			id: "kdjf8uif",
-			title_suggestion:
-				"ipsum dolor sit amet consectetur adipisicing elit. Voluptates, est ullam enim debitis dolorem tempora cum deserunt voluptatum sunt quibusdam?",
-			list: [
-				{
-					id: "skdjfksdjfksj",
-					suggestion:
-						"ipsum dolor sit amet consectetur adipisicing elit. Voluptates, est ullam enim debitis dolorem tempora cum deserunt voluptatum sunt quibusdam?",
-				},
-				{
-					id: "skdjfksdfdfdjfksj",
-					suggestion:
-						"dolor sit amet consectetur adipisicing elit. Voluptates, est ullam enim debitis dolorem tempora",
-				},
-				{
-					id: "skdjfksd23fdfdjfksj",
-					suggestion:
-						"dolor sit amet consectetur adipisicing elit. Voluptates, est ullam enim debitis dolorem tempora",
-				},
-			],
-		},
-	];
-
 	return (
-		<div className="border bg-[#25966b23] flex items-start gap-5  border-[#2aa2747a] flex-1 rounded-2xl shadow-md p-4">
-			<div className="h-10 w-10 rounded-full shadow-md gradient-upgrade aspect-square">
-				<Image
-					className={`${
-						(loading && "ai-spin") || (!isTypeWriterComplete && "ai-spin")
-					} rounded-full h-10 w-10 p-2`}
-					src={suggestionImage}
-					alt="ai suggestion"
-					height={25}
-					width={25}
-				/>
-			</div>
-			{list_suggestions.map((suggestions) => {
-				return (
-					<ul key={suggestions.id} className="flex flex-col gap-2">
-						<li className="mb-4 font-medium">
-							<TypeWrite
-								fontSize={19}
-								text={suggestions.title_suggestion}
-								key={suggestions.id}
-								showCaret={false}
-								color="white"
-								typingSpeed={20}
-								setIsTypeWriterComplete={setIsTypeWriterComplete}
-								timeToStartNewText={20}
-							/>
-						</li>
-						{suggestions.list
-							.slice(
-								0,
-								showMore === detailsLength
-									? detailsLength
-									: showMoreAiSugestions
-							)
-							.map((list_suggestions) => {
-								return (
-									<li
-										className={`list-disc list-outside text-[15px] text-[#ffffffe7] font-normal`}
-										key={list_suggestions.id}
-									>
-										<TypeWrite
-											fontSize={16}
-											text={list_suggestions.suggestion}
+		<div
+			className={`
+				 before:opacity-0 before:hover:opacity-40 w-full before:z-[-1] after:z-[-1] after:absolute after:top-[-1px] after:left-[-1px] before:rounded-2xl after:rounded-2xl rounded-2xl before:absolute before:top-[-1px] before:left-[-1px] bg-transparent relative bg-gradient-to-tr from-[#050e0b] to-[#000000] justify-between custom-block text-text-normal text-white font-medium flex items-center gap-2 glow4
+							`}
+		>
+			<div className="bg-[#25966b23] flex items-start gap-5 flex-1 rounded-2xl shadow-md p-4">
+				<div className="h-10 w-10 rounded-full shadow-md gradient-upgrade aspect-square">
+					<Image
+						className={`${
+							(loading && "ai-spin") || (!isTypeWriterComplete && "ai-spin")
+						} rounded-full h-10 w-10 p-2`}
+						src={suggestionImage}
+						alt="ai suggestion"
+						height={25}
+						width={25}
+					/>
+				</div>
+				{list_suggestions.map((suggestions) => {
+					return (
+						<ul key={suggestions.id} className="flex flex-col gap-2">
+							<li className="font-medium mb-3">
+								<TypeWrite
+									fontSize={19}
+									text={suggestions.title_suggestion}
+									key={suggestions.id}
+									showCaret={false}
+									color="white"
+									typingSpeed={20}
+									setIsTypeWriterComplete={setIsTypeWriterComplete}
+									timeToStartNewText={20}
+								/>
+							</li>
+							{suggestions.list
+								.slice(
+									0,
+									showMore === detailsLength
+										? detailsLength
+										: showMoreAiSugestions
+								)
+								.map((list_suggestions) => {
+									return (
+										<li
+											className={`list-disc list-outside text-[15px] text-[#ffffffe7] font-normal`}
 											key={list_suggestions.id}
-											showCaret={false}
-											color="white"
-											typingSpeed={20}
-											setIsTypeWriterComplete={setIsTypeWriterComplete}
-											timeToStartNewText={20}
-										/>
-									</li>
-								);
-							})}
-					</ul>
-				);
-			})}
+										>
+											<TypeWrite
+												fontSize={16}
+												text={list_suggestions.suggestion}
+												key={list_suggestions.id}
+												showCaret={false}
+												color="white"
+												typingSpeed={20}
+												setIsTypeWriterComplete={setIsTypeWriterComplete}
+												timeToStartNewText={20}
+											/>
+										</li>
+									);
+								})}
+						</ul>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
