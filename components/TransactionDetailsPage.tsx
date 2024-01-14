7aa8cc2a93446d96a4d77a0add536c87a607f43a
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-14 02:00:10 +0000

    feat:emphasize on button when there is more suggestion from ai

--- components/TransactionDetailsPage.tsx
+++ components/TransactionDetailsPage.tsx
@@ -28,6 +28,31 @@ const TransactionDetails = ({ transData }: { transData: Data[] }) => {
 	// 	}
 	// };
 
+	const list_suggestions = [
+		{
+			id: "kdjf8uif",
+			title_suggestion:
+				"ipsum dolor sit amet consectetur adipisicing elit. Voluptates, est ullam enim debitis dolorem tempora cum deserunt voluptatum sunt quibusdam?",
+			list: [
+				{
+					id: "skdjfksdjfksj",
+					suggestion:
+						"ipsum dolor sit amet consectetur adipisicing elit. Voluptates, est ullam enim debitis dolorem tempora cum deserunt voluptatum sunt quibusdam?",
+				},
+				{
+					id: "skdjfksdfdfdjfksj",
+					suggestion:
+						"dolor sit amet consectetur adipisicing elit. Voluptates, est ullam enim debitis dolorem tempora",
+				},
+				{
+					id: "skdjfksd23fdfdjfksj",
+					suggestion:
+						"dolor sit amet consectetur adipisicing elit. Voluptates, est ullam enim debitis dolorem tempora",
+				},
+			],
+		},
+	];
+
 	return (
 		<main className=" flex flex-col p-5 lg:flex-row gap-5 justify-center w-full relative z-0">
 			<div className="text-white w-full flex flex-col gap-5 flex-[2]">
@@ -37,6 +62,7 @@ const TransactionDetails = ({ transData }: { transData: Data[] }) => {
 						detailsLength={detailsLength}
 						showMore={showMore}
 						loading={loading}
+						list_suggestions={list_suggestions}
 					/>
 				</div>
 				{Object.entries(transData).map(([key, value]) => {
@@ -94,20 +120,29 @@ const TransactionDetails = ({ transData }: { transData: Data[] }) => {
 							<div className="flex flex-wrap md:flex-nowrap lg:flex-wrap xl:flex-nowrap mt-7 items-center gap-5  justify-between">
 								<button
 									disabled={showMore === Object.keys(value).length - 1} // -1 = amount value
-									className={`p-3  shadow-lg w-full disabled:active:scale-100 disabled:border-none disabled:bg-[#80808062] transition-all duration-150 active:scale-[1.01] bg-brand-green-darker hover:bg-mid--yellow border hover:border-transparent   rounded-lg text-white`}
+									className={`disabled:bg-gradient-to-t disabled:from-[#80808062] disabled:to-[#80808062]  before:opacity-0 hover:before:opacity-30 p-3 w-full before:z-[-1] after:z-[-1] after:absolute after:top-[-1px] after:left-[-1px] before:rounded-lg after:rounded-lg rounded-lg before:absolute before:top-[-1px] before:left-[-1px] relative bg-gradient-to-tr from-[#050e0b] to-[#000000] custom-block text-text-normal text-white font-medium ${
+										showMoreAiSugestions === list_suggestions[0].list.length
+											? "bg-gradient-to-tr transition-colors duration-150 ease-in from-[#15744f] to-[#15744f] hover:bg-gradient-to-tr hover:from-[#29a173cc] hover:to-[#29a173cc]"
+											: "glow2"
+									}`}
 									type="button"
 									onClick={() => {
 										if (showMore === Object.keys(value).length - 1) {
 											return;
 										} else {
 											setShowMore(showMore + 5);
-											setShowMoreAiSugestions(showMoreAiSugestions + 1);
+											if (
+												showMoreAiSugestions !== list_suggestions[0].list.length
+											) {
+												setShowMoreAiSugestions(showMoreAiSugestions + 1);
+											}
 											setDetailsLength(Object.keys(value).length - 1);
 										}
 									}}
 								>
 									Show More Details
 								</button>
+
 								<button
 									disabled={showMore === 5}
 									className={`p-3 shadow-md transition-colors duration-150 disabled:active:scale-100 active:scale-[1.01] disabled:bg-[#80808062] bg-brand-green-darker w-full hover:bg-mid--yellow rounded-lg text-white`}
@@ -134,6 +169,7 @@ const TransactionDetails = ({ transData }: { transData: Data[] }) => {
 						detailsLength={detailsLength}
 						showMore={showMore}
 						loading={loading}
+						list_suggestions={list_suggestions}
 					/>
 				</div>
 
