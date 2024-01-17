f2e61b3b345f51b5211500be506d4c9a385a7d9d
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-17 01:01:01 +0000

    refactor: change data structure for Ai response and implement a typeWriter effect on each data returned

--- components/IncomingMessage.tsx
+++ components/IncomingMessage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import chatbot from "@/public/chatbot.svg";
 import Image from "next/image";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
@@ -18,81 +18,142 @@ import TypeWrite from "./TypeWrite";
 interface IcomingProp {
 	fontSize?: number;
 	typeWrite?: boolean;
-	message: {
-		id: string;
-		firstText: string;
-		timeStamp?: string;
-		list?: { id: string; title?: string; msg: string }[];
-		endingText: string;
-	};
+	list: { id: string; msg: string }[];
+	firstText: string;
+	endingText: string;
 }
 
-const IncomingMessage = ({ message, fontSize, typeWrite }: IcomingProp) => {
+type IcurrentSection = "firstText" | "list" | "endingText";
+
+const IncomingMessage = ({
+	list,
+	firstText,
+	fontSize,
+	typeWrite,
+	endingText,
+}: IcomingProp) => {
+	const [currentSection, setCurrentSection] =
+		useState<IcurrentSection>("firstText");
+	const [isTypeWriterComplete, setIsTypeWriterComplete] = useState(false);
+
+	useEffect(() => {
+		const handleNextSection = () => {
+			setCurrentSection((prevSection): IcurrentSection => {
+				switch (prevSection) {
+					case "firstText":
+						if (isTypeWriterComplete) {
+							setCurrentSection("list");
+						}
+						return "firstText";
+					case "list":
+						if (isTypeWriterComplete) {
+							setCurrentSection("endingText");
+						}
+						return "list";
+					case "endingText":
+						return "endingText";
+					default:
+						return currentSection;
+				}
+			});
+		};
+
+		handleNextSection();
+	}, [currentSection, isTypeWriterComplete]);
+
 	return (
 		<>
 			{typeWrite ? (
 				<div className="flex items-end mr-auto gap-3">
 					<Image src={chatbot} height={35} width={35} alt="satsat-ai" />
-					<div className=" flex inMessage w-full shadow-lg flex-col gap-1 p-4 rounded-3xl text-white">
-						<TypeWrite
-							text={message.firstText}
-							fontSize={fontSize}
-							color="white"
-							showCaret={true}
-							timeToStartNewText={1000}
-							typingSpeed={20}
-						/>
-
-						{message.list && (
-							<ul className="unordered-list">
-								{message.list.map((listMessage) => {
-									return (
-										<li
-											style={{
-												fontSize: `${fontSize ? fontSize : 16}px`,
-											}}
-											className="list-disc list-inside"
-											key={listMessage.id}
-										>
-											<TypeWrite
-												text={listMessage.msg}
-												fontSize={fontSize}
-												color="white"
-												showCaret={true}
-												timeToStartNewText={1000}
-												typingSpeed={20}
-											/>
-										</li>
-									);
-								})}
-							</ul>
-						)}
-						{message.endingText && (
+					<div className=" flex inMessage md:max-w-full w-full shadow-lg flex-col gap-1 p-4 rounded-3xl text-white">
+						{currentSection === "firstText" && (
 							<TypeWrite
-								text={message.endingText}
+								text={firstText}
 								fontSize={fontSize}
 								color="white"
-								showCaret={true}
-								timeToStartNewText={1000}
+								showCaret={false}
+								timeToStartNewText={firstText?.length}
 								typingSpeed={20}
+								setIsTypeWriterComplete={setIsTypeWriterComplete}
+								fontWeight="medium"
 							/>
 						)}
+						{list && currentSection === "list" && (
+							<>
+								<p className="font-medium">{firstText}</p>
+								<ul className="unordered-list">
+									{list.map((listMessage: { id: string; msg: string }) => {
+										return (
+											<li
+												style={{
+													fontSize: `${fontSize ? fontSize : 16}px`,
+												}}
+												className="list-disc list-inside"
+												key={listMessage.id}
+											>
+												<TypeWrite
+													text={listMessage.msg}
+													fontSize={fontSize}
+													color="white"
+													showCaret={false}
+													timeToStartNewText={listMessage.msg?.length}
+													typingSpeed={20}
+													fontWeight="medium"
+												/>
+											</li>
+										);
+									})}
+								</ul>
+							</>
+						)}
+						{endingText && currentSection === "endingText" && (
+							<>
+								<p className="font-medium">{firstText}</p>
+								<ul className="unordered-list">
+									{list.map((listMessage: { id: string; msg: string }) => {
+										return (
+											<li
+												style={{
+													fontSize: `${fontSize ? fontSize : 16}px`,
+												}}
+												className="list-disc list-inside"
+												key={listMessage.id}
+											>
+												<p>{listMessage.msg}</p>
+											</li>
+										);
+									})}
+								</ul>
+								<TypeWrite
+									text={endingText}
+									fontSize={fontSize}
+									color="white"
+									showCaret={false}
+									typingSpeed={15}
+									timeToStartNewText={10}
+									fontWeight="medium"
+								/>
+							</>
+						)}
+
 						<div className="flex items-center gap-3 mt-3">
 							<ContentCopyIcon
 								fontSize="small"
 								className="active:scale-[1.03] cursor-pointer"
 								color="inherit"
 							/>
-							{/* <ThumbUpAltIcon
-						fontSize="small"
-						className="active:scale-[1.03] cursor-pointer"
-						color="inherit"
-					/>
-					<ThumbDownIcon
-						fontSize="small"
-						className="active:scale-[1.03] cursor-pointer"
-						color="inherit"
-					/> */}
+
+							<ThumbUpAltIcon
+								fontSize="small"
+								className="active:scale-[1.03] cursor-pointer"
+								color="inherit"
+							/>
+							<ThumbDownIcon
+								fontSize="small"
+								className="active:scale-[1.03] cursor-pointer"
+								color="inherit"
+							/>
 						</div>
 					</div>
 				</div>
@@ -100,19 +161,19 @@ const IncomingMessage = ({ message, fontSize, typeWrite }: IcomingProp) => {
 				<div className="flex w-full items-end mr-auto gap-3">
 					<Image src={chatbot} height={35} width={35} alt="satsat-ai" />
 					<div className=" flex inMessage shadow-lg flex-col gap-1 p-4 rounded-3xl text-white">
-						{message.firstText && (
+						{firstText && (
 							<p
 								style={{
 									fontSize: `${fontSize ? fontSize : 16}px`,
 								}}
 							>
-								{message.firstText}
+								{firstText}
 							</p>
 						)}
 
-						{message.list && (
+						{list && (
 							<ul className="unordered-list">
-								{message.list.map((listMessage) => {
+								{list.map((listMessage) => {
 									return (
 										<li
 											style={{
@@ -127,13 +188,13 @@ const IncomingMessage = ({ message, fontSize, typeWrite }: IcomingProp) => {
 								})}
 							</ul>
 						)}
-						{message.endingText && (
+						{endingText && (
 							<p
 								style={{
 									fontSize: `${fontSize ? fontSize : 16}px`,
 								}}
 							>
-								{message.endingText}
+								{endingText}
 							</p>
 						)}
 						<div className="flex items-center gap-3 mt-3">
@@ -142,16 +203,16 @@ const IncomingMessage = ({ message, fontSize, typeWrite }: IcomingProp) => {
 								className="active:scale-[1.03] cursor-pointer"
 								color="inherit"
 							/>
-							{/* <ThumbUpAltIcon
-						fontSize="small"
-						className="active:scale-[1.03] cursor-pointer"
-						color="inherit"
-					/>
-					<ThumbDownIcon
-						fontSize="small"
-						className="active:scale-[1.03] cursor-pointer"
-						color="inherit"
-					/> */}
+							<ThumbUpAltIcon
+								fontSize="small"
+								className="active:scale-[1.03] cursor-pointer"
+								color="inherit"
+							/>
+							<ThumbDownIcon
+								fontSize="small"
+								className="active:scale-[1.03] cursor-pointer"
+								color="inherit"
+							/>
 						</div>
 					</div>
 				</div>
