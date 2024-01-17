42eb043507f474ee0882db659cdadb7db25ae030
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-01-17 12:36:49 +0000

    Fix: overflow on smaller devices on initial page

--- components/ChatMain.tsx
+++ components/ChatMain.tsx
@@ -184,16 +184,19 @@ const ChatMain = ({ chatContainerId }: { chatContainerId?: string }) => {
 		};
 	}, [conversations]);
 
+	// const onTypingChange = () => {
+	// 	handleScrollToBottom();
+	// };
 	return (
 		<>
-			{conversations.length >= 1 ? (
-				<main
-					ref={chatContainerRef}
-					className=" text-white w-full h-full
-			custom-scroll2 relative overflow-y-auto"
-				>
-					<ToggleSidebars />
-					<div className=" flex flex-col gap-5 p-5">
+			<main
+				ref={chatContainerRef}
+				className=" text-white w-full h-full
+					custom-scroll2 relative flex flex-col justify-between overflow-y-auto"
+			>
+				<ToggleSidebars />
+				{conversations.length >= 1 ? (
+					<div className=" flex flex-col gap-5 pt-5 px-5">
 						<div className="fixed bottom-24 -translate-x-1/2 left-1/2">
 							{scrollToTop && isOldConversation && (
 								<ChatScrolltoTop scrolltoTop={scrolltoTop} />
@@ -220,130 +223,223 @@ const ChatMain = ({ chatContainerId }: { chatContainerId?: string }) => {
 										list={conversation.list!}
 										firstText={conversation.firstText as string}
 										typeWrite={true}
+										// onTypingChange={onTypingChange}
 									/>
 								);
 							}
 						})}
-					</div>
-				</main>
-			) : (
-				<main className=" h-full">
-					<ToggleSidebars />
 
-					<div className="w-full flex  flex-col h-full items-center justify-center pb-10 overflow-y-auto custom-scroll">
-						<h1 className="text-white text-text-40 mb-0 text-center sm:text-text-60">
-							SATSAT AI
-						</h1>
-						<div className="text-white mt-10 max-w-xs md:max-w-2xl mx-auto">
-							<h2 className="font-medium mx-auto w-fit xl:mr-auto xl:w-full">
-								Chat suggestions
-							</h2>
-							<div className="flex flex-wrap xl:grid xl:grid-cols-2 justify-center gap-5">
-								{chatSuggestions.map((suggestions) => {
-									return (
-										<button
-											key={suggestions.id}
-											type="button"
-											className="border border-white py-2 px-4 text-text-12 sm:text-text-normal hover:bg-brand-green/20 active:scale-[1.01] rounded-3xl"
-										>
-											{suggestions.value}
-										</button>
-									);
-								})}
+						<form
+							onSubmit={handleSubmit(onSubmit)}
+							className="text-white sticky bottom-3 p-5 mt-auto"
+						>
+							<div className="bg-[#071f07] rounded-lg max-w-3xl mx-auto">
+								<div
+									tabIndex={0}
+									className="flex  w-full mt-auto border border-white items-center p-1 justify-between rounded-lg px-2 gap-5"
+								>
+									<textarea
+										disabled={loading}
+										rows={1}
+										onInput={(e) => handleTextAreaResize(e)}
+										autoFocus
+										autoCorrect="true"
+										onKeyDown={(e) => (
+											handleKeyDown(e), handleTextAreaResize(e)
+										)}
+										className="w-full text-text-normal scrollbar-hidden placeholder:text-white/70 placeholder:text-text-normal outline-none bg-brand-green border-none h-auto bg-transparent"
+										placeholder="Message SATSAT AI..."
+										{...register("userMessage", {
+											required: false,
+										})}
+									/>
+									<button type="submit" disabled={loading}>
+										<TelegramIcon
+											tabIndex={0}
+											fontSize="large"
+											className="active:scale-[1.02]"
+											color="inherit"
+											aria-hidden="false"
+										/>
+									</button>
+								</div>
+							</div>
+							<div ref={helpOptionsRef}>
+								{showHelpOptions && (
+									<div className="absolute bottom-36 right-16 bg-[#062506] shadow-md w-[200px] p-3 rounded-lg">
+										<ul className="flex cursor-pointer items-start w-full text-[14px] font-medium flex-col gap-1 ">
+											<li
+												onClick={() => setShowHelpOptions(false)}
+												className=" text-[13px] hover:bg-brand-green text-white active:scale-[1.03] p-2 w-full rounded-lg"
+											>
+												<Link
+													target="_blank"
+													href="/faq"
+													className="flex gap-2 items-center"
+												>
+													<HiOutlineExternalLink size={20} />
+													Help & FAQ
+												</Link>
+											</li>
+											<li
+												onClick={() => setShowHelpOptions(false)}
+												className="text-[13px] hover:bg-brand-green text-white active:scale-[1.03] p-2 w-full rounded-lg"
+											>
+												<Link
+													target="_blank"
+													href={"/contact"}
+													className="flex gap-2 items-center"
+												>
+													<HiOutlineExternalLink size={20} />
+													Contact Support
+												</Link>
+											</li>
+											<li
+												onClick={() => setShowHelpOptions(false)}
+												className="text-[13px] hover:bg-brand-green text-white active:scale-[1.03] p-2 w-full rounded-lg"
+											>
+												<Link
+													target="_blank"
+													href={"/terms-and-conditions"}
+													className="flex items-center  gap-2"
+												>
+													<HiOutlineExternalLink size={20} />
+													Terms & Conditions
+												</Link>
+											</li>
+										</ul>
+									</div>
+								)}
+								{conversations.length < 1 && (
+									<div
+										onClick={() => setShowHelpOptions((prev) => !prev)}
+										className="absolute active:scale-[1.04] bottom-24 cursor-pointer transition-colors duration-200 p-2 w-fit hover:bg-grey-light rounded-full bg-brand-green-darker right-16 "
+									>
+										<IoMdHelpCircleOutline color="white" size={25} />
+									</div>
+								)}
+							</div>
+						</form>
+					</div>
+				) : (
+					<>
+						<div className="w-full flex  flex-col h-full items-center justify-center overflow-y-auto custom-scroll">
+							<h1 className="text-white text-text-40 mb-0 text-center sm:text-text-60">
+								SATSAT AI
+							</h1>
+							<div className="text-white mt-10 max-w-xs md:max-w-2xl mx-auto">
+								<h2 className="font-medium mx-auto w-fit xl:mr-auto xl:w-full">
+									Chat suggestions
+								</h2>
+								<div className="flex flex-wrap xl:grid xl:grid-cols-2 justify-center gap-5">
+									{chatSuggestions.map((suggestions) => {
+										return (
+											<button
+												key={suggestions.id}
+												type="button"
+												className="border border-white py-2 px-4 text-text-12 sm:text-text-normal hover:bg-brand-green/20 active:scale-[1.01] rounded-3xl"
+											>
+												{suggestions.value}
+											</button>
+										);
+									})}
+								</div>
 							</div>
 						</div>
-					</div>
-				</main>
-			)}
-			<form
-				onSubmit={handleSubmit(onSubmit)}
-				className="text-white sticky bottom-3 p-5 mt-auto"
-			>
-				<div className="bg-[#071f07] rounded-lg max-w-3xl mx-auto">
-					<div
-						tabIndex={0}
-						className="flex  w-full mt-auto border border-white items-center p-1 justify-between rounded-lg px-2 gap-5"
-					>
-						<textarea
-							disabled={loading}
-							rows={1}
-							onInput={(e) => handleTextAreaResize(e)}
-							autoFocus
-							autoCorrect="true"
-							onKeyDown={(e) => (handleKeyDown(e), handleTextAreaResize(e))}
-							className="w-full text-text-normal scrollbar-hidden placeholder:text-white/70 placeholder:text-text-normal outline-none bg-brand-green border-none h-auto bg-transparent"
-							placeholder="Message SATSAT AI..."
-							{...register("userMessage", {
-								required: false,
-							})}
-						/>
-						<button type="submit" disabled={loading}>
-							<TelegramIcon
-								tabIndex={0}
-								fontSize="large"
-								className="active:scale-[1.02]"
-								color="inherit"
-								aria-hidden="false"
-							/>
-						</button>
-					</div>
-				</div>
-				<div ref={helpOptionsRef}>
-					{showHelpOptions && (
-						<div className="absolute bottom-36 right-16 bg-[#062506] shadow-md w-[200px] p-3 rounded-lg">
-							<ul className="flex cursor-pointer items-start w-full text-[14px] font-medium flex-col gap-1 ">
-								<li
-									onClick={() => setShowHelpOptions(false)}
-									className=" text-[13px] hover:bg-brand-green text-white active:scale-[1.03] p-2 w-full rounded-lg"
-								>
-									<Link
-										target="_blank"
-										href="/faq"
-										className="flex gap-2 items-center"
-									>
-										<HiOutlineExternalLink size={20} />
-										Help & FAQ
-									</Link>
-								</li>
-								<li
-									onClick={() => setShowHelpOptions(false)}
-									className="text-[13px] hover:bg-brand-green text-white active:scale-[1.03] p-2 w-full rounded-lg"
-								>
-									<Link
-										target="_blank"
-										href={"/contact"}
-										className="flex gap-2 items-center"
-									>
-										<HiOutlineExternalLink size={20} />
-										Contact Support
-									</Link>
-								</li>
-								<li
-									onClick={() => setShowHelpOptions(false)}
-									className="text-[13px] hover:bg-brand-green text-white active:scale-[1.03] p-2 w-full rounded-lg"
-								>
-									<Link
-										target="_blank"
-										href={"/terms-and-conditions"}
-										className="flex items-center  gap-2"
-									>
-										<HiOutlineExternalLink size={20} />
-										Terms & Conditions
-									</Link>
-								</li>
-							</ul>
-						</div>
-					)}
-					{conversations.length < 1 && (
-						<div
-							onClick={() => setShowHelpOptions((prev) => !prev)}
-							className="absolute active:scale-[1.04] bottom-24 cursor-pointer transition-colors duration-200 p-2 w-fit hover:bg-grey-light rounded-full bg-brand-green-darker right-16 "
+						<form
+							onSubmit={handleSubmit(onSubmit)}
+							className="text-white sticky bottom-3 p-5 mt-auto"
 						>
-							<IoMdHelpCircleOutline color="white" size={25} />
-						</div>
-					)}
-				</div>
-			</form>
+							<div className="bg-[#071f07] rounded-lg max-w-3xl mx-auto">
+								<div
+									tabIndex={0}
+									className="flex  w-full mt-auto border border-white items-center p-1 justify-between rounded-lg px-2 gap-5"
+								>
+									<textarea
+										disabled={loading}
+										rows={1}
+										onInput={(e) => handleTextAreaResize(e)}
+										autoFocus
+										autoCorrect="true"
+										onKeyDown={(e) => (
+											handleKeyDown(e), handleTextAreaResize(e)
+										)}
+										className="w-full text-text-normal scrollbar-hidden placeholder:text-white/70 placeholder:text-text-normal outline-none bg-brand-green border-none h-auto bg-transparent"
+										placeholder="Message SATSAT AI..."
+										{...register("userMessage", {
+											required: false,
+										})}
+									/>
+									<button type="submit" disabled={loading}>
+										<TelegramIcon
+											tabIndex={0}
+											fontSize="large"
+											className="active:scale-[1.02]"
+											color="inherit"
+											aria-hidden="false"
+										/>
+									</button>
+								</div>
+							</div>
+							<div ref={helpOptionsRef}>
+								{showHelpOptions && (
+									<div className="absolute bottom-36 right-16 bg-[#062506] shadow-md w-[200px] p-3 rounded-lg">
+										<ul className="flex cursor-pointer items-start w-full text-[14px] font-medium flex-col gap-1 ">
+											<li
+												onClick={() => setShowHelpOptions(false)}
+												className=" text-[13px] hover:bg-brand-green text-white active:scale-[1.03] p-2 w-full rounded-lg"
+											>
+												<Link
+													target="_blank"
+													href="/faq"
+													className="flex gap-2 items-center"
+												>
+													<HiOutlineExternalLink size={20} />
+													Help & FAQ
+												</Link>
+											</li>
+											<li
+												onClick={() => setShowHelpOptions(false)}
+												className="text-[13px] hover:bg-brand-green text-white active:scale-[1.03] p-2 w-full rounded-lg"
+											>
+												<Link
+													target="_blank"
+													href={"/contact"}
+													className="flex gap-2 items-center"
+												>
+													<HiOutlineExternalLink size={20} />
+													Contact Support
+												</Link>
+											</li>
+											<li
+												onClick={() => setShowHelpOptions(false)}
+												className="text-[13px] hover:bg-brand-green text-white active:scale-[1.03] p-2 w-full rounded-lg"
+											>
+												<Link
+													target="_blank"
+													href={"/terms-and-conditions"}
+													className="flex items-center  gap-2"
+												>
+													<HiOutlineExternalLink size={20} />
+													Terms & Conditions
+												</Link>
+											</li>
+										</ul>
+									</div>
+								)}
+								{conversations.length < 1 && (
+									<div
+										onClick={() => setShowHelpOptions((prev) => !prev)}
+										className="absolute active:scale-[1.04] bottom-24 cursor-pointer transition-colors duration-200 p-2 w-fit hover:bg-grey-light rounded-full bg-brand-green-darker right-16 "
+									>
+										<IoMdHelpCircleOutline color="white" size={25} />
+									</div>
+								)}
+							</div>
+						</form>
+					</>
+				)}
+			</main>
 		</>
 	);
 };
