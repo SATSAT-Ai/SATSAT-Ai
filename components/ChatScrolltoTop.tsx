b5d5dc80f9683719395ded070af63a0fd3b5f4ca
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-02-01 03:10:24 +0000

    style:change scroller  backgroundColor

--- components/ChatScrolltoTop.tsx
+++ components/ChatScrolltoTop.tsx
@@ -9,7 +9,7 @@ const ChatScrolltoTop = ({ scrolltoTop }: { scrolltoTop: () => void }) => {
 			onClick={scrolltoTop}
 			className={`  left-2/3 w-fit ml-auto text-white  cursor-pointer`}
 		>
-			<div className="h-12 w-12 grid active:scale-[1.03] ml-auto bg-grey-light/70 rounded-full justify-center items-center">
+			<div className="h-12 w-12 grid active:scale-[1.03] ml-auto bg-brand-green/70 rounded-full justify-center items-center">
 				<NorthIcon fontSize="small" color="inherit" />
 			</div>
 		</button>
