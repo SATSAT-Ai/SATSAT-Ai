0404588c7a81fcfdbc8feb2db925d72d51762e3b
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-02-18 02:48:47 +0000

    feat:redirect to documentation if /docs/* is not a found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,21 @@
+"use client";
+
 import NotfoundImage from "../public/Notfound.svg";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function NotFound() {
+	const router = useRouter();
+	const pathname = usePathname();
+
+	const redirectToDocs = () => {
+		router.push("/docs/getting-started");
+	};
+
+	const redirectToHome = () => {
+		router.push("/");
+	};
+
 	return (
 		<main className="bg-darker min-h-screen w-full relative pb-20">
 			<div className="flex items-center justify-center w-full h-full pt-20">
@@ -11,12 +24,22 @@ export default function NotFound() {
 			<p className="text-mid--yellow text-center py-5">
 				The page you are looking for cannot be found.
 			</p>
-			<Link
-				href="/"
-				className=" active:scale-[1.01] button w-fit hover:bg-brand-green border border-white block mx-auto transition-all text-white hover:border-none"
-			>
-				Go back home
-			</Link>
+
+			{pathname?.startsWith("/docs/") || pathname === "/docs" ? (
+				<button
+					className=" active:scale-[1.01] button w-fit hover:bg-brand-green border border-white block mx-auto transition-all text-white hover:border-none"
+					onClick={redirectToDocs}
+				>
+					Head to Documentation{" "}
+				</button>
+			) : (
+				<button
+					className=" active:scale-[1.01] button w-fit hover:bg-brand-green border border-white block mx-auto transition-all text-white hover:border-none"
+					onClick={redirectToHome}
+				>
+					Back to Home
+				</button>
+			)}
 		</main>
 	);
 }
--- app/profile/page.tsx
+++
@@ -1,5 +0,0 @@
-const page = () => {
-	return <div>Profile page</div>;
-};
-
-export default page;
