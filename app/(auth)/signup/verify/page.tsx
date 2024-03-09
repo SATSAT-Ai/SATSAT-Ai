42b70b596045965c6d99812d29d2379b1a3d7161
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-03-09 23:03:38 +0000

    refactor:change metadata title and separate client code from server

--- app/(auth)/signup/verify/page.tsx
+++ app/(auth)/signup/verify/page.tsx
@@ -1,39 +1,24 @@
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import VerifyAccount from "@/components/VerifyAccount";
-import Logo from "@/public/satsat-logo.svg";
-import Image from "next/image";
+import VerificationStages from "@/components/VerificationStages";
+import { Metadata } from "next";
 
-const page = () => {
+export const metadata: Metadata = {
+	title: "SatSat-Ai User Verification",
+};
+
+const Page = () => {
 	return (
 		<>
 			<Header />
-			<div className=" flex relative overflow-x-clip items-center justify-center min-h-screen bg-darker">
-				<div className=" animate-pulse green-blob2 w-96 h-96 top-[-30%] lg:top-[-30%] left-[50%] "></div>
-				<main>
-					<div className="my-max">
-						<div className="max-w-xs mx-auto">
-							<Image
-								src={Logo}
-								className="mx-auto"
-								height={130}
-								width={130}
-								alt="SATSAT-Ai"
-								priority
-							/>
-							<p className="text-mid--yellow text-center my-6">
-								Enter the signup code we just sent to
-								{/* render dynamic email address */}
-								{" kamasahdickson19@gmail.com"}
-							</p>
-							<VerifyAccount />
-						</div>
-					</div>
+			<div className=" pt-20 flex relative overflow-x-clip items-center justify-center min-h-screen bg-darker">
+				<div className=" animate-pulse green-blob2 w-96 h-96 top-[-50%] lg:top-[-30%] left-[50%] "></div>
+
+				<main className="my-max">
+					<VerificationStages />
 				</main>
 			</div>
-			<Footer />
 		</>
 	);
 };
 
-export default page;
+export default Page;
