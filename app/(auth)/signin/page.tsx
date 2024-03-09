2707910a9676c720991639968d02b504f70881be
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-03-09 22:47:30 +0000

    refactor:split client code from server

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import signinImage from "@/public/signinImage.svg";
+import signInImage from "@/public/signinImage.svg";
 import { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import SigninForm from "@/components/SigninForm";
+import SignInForm from "@/components/SignInForm";
 
 export const metadata: Metadata = {
-	title: "Welcome back to SATSAT AI",
+	title: "Welcome back to SatSat-Ai",
 };
 
 const page = () => {
@@ -17,11 +17,11 @@ const page = () => {
 				<div className="yellow-blob3 w-96 h-96 top-[-40%] left-[20%] md:top-[-25%] md:left-[10%] animate-pulse"></div>
 
 				<main className="grid grid-cols-1 lg:grid-cols-2 h-auto mt-20 items-center justify-between w-full gap-5 max-w-5xl xl:max-w-6xl my-max">
-					<SigninForm />
+					<SignInForm />
 					<div className="rounded-[40px] hidden lg:flex overflow-clip">
 						<Image
 							className="object-cover rounded-[40px]"
-							src={signinImage}
+							src={signInImage}
 							height={550}
 							width={550}
 							alt="signup"
