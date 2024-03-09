5da31374e7329942f38997e0e77f9f86e90dcdf5
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-03-09 22:50:54 +0000

    refactor:split client code from server

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -4,10 +4,10 @@ import signupImage from "@/public/signupImage.svg";
 import { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import SignupForm from "@/components/SignupForm";
+import SignUpForm from "@/components/SignUpForm";
 
 export const metadata: Metadata = {
-	title: "SATSAT-AI Get started!",
+	title: "SatSat-Ai Get started!",
 };
 const page = () => {
 	return (
@@ -16,8 +16,8 @@ const page = () => {
 			<div className="bg-darker pb-7 w-full min-h-screen items-center flex relative">
 				<div className="green-blob2 w-96 h-96 top-[-40%] left-[20%] md:top-[-25%] md:left-[10%] animate-pulse"></div>
 
-				<main className="grid grid-cols-1 lg:grid-cols-2 h-auto mt-20 items-center justify-between w-full gap-5 max-w-5xl xl:max-w-6xl my-max">
-					<SignupForm />
+				<main className="grid grid-cols-1 lg:grid-cols-2 h-auto mt-24 items-center justify-between w-full gap-5 max-w-5xl xl:max-w-6xl my-max">
+					<SignUpForm />
 					<div className="rounded-[40px] hidden lg:flex overflow-clip">
 						<Image
 							className="object-cover rounded-[40px]"
