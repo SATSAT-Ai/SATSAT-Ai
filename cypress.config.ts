3ba641cce4b9a9ad0d7bcb0d83995da69d98fe1a
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-03-19 00:41:15 +0000

    feat:configure cypress test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	projectId: "23becv",
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+		baseUrl: "http://localhost:3000",
+		chromeWebSecurity: false,
+		pageLoadTimeout: 100000,
+		defaultCommandTimeout: 60000,
+		requestTimeout: 40000,
+	},
 });
