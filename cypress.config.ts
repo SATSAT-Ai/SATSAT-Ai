858f83355ef348443c449d18c3b4bf1904f3180a
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-03-22 04:52:35 +0000

    refactor: change baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
-		baseUrl: "https://test.satsat-ai.vercel.app",
+		baseUrl: "http://localhost:3000",
 		chromeWebSecurity: false,
 		pageLoadTimeout: 90000,
 		defaultCommandTimeout: 40000,
