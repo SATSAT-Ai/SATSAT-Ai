bda455a1ae79d634ec034f48e0ad6e1d3b3d829e
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-03-30 23:01:16 +0000

    style: add overflow-clip and height to be viewport height

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -13,10 +13,8 @@ const page = () => {
 	return (
 		<>
 			<Header />
-			<div className="bg-darker pb-7 w-full min-h-screen items-center flex relative">
-				<div className="yellow-blob3 w-96 h-96 top-[-40%] left-[20%] md:top-[-25%] md:left-[10%] animate-pulse"></div>
-
-				<main className="grid grid-cols-1 lg:grid-cols-2 h-auto mt-20 items-center justify-between w-full gap-5 max-w-5xl xl:max-w-6xl my-max">
+			<div className="bg-darker overflow-clip  pb-7 w-full min-h-screen items-center flex relative">
+				<main className="grid grid-cols-1 lg:grid-cols-2 h-screen mt-20 items-center justify-between w-full gap-5 max-w-5xl xl:max-w-6xl my-max">
 					<SignInForm />
 					<div className="rounded-[40px] hidden lg:flex overflow-clip">
 						<Image
