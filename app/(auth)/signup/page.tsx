f09d8caaca2dfc8eca0394459b44f2ff10b279e5
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-03-30 23:05:29 +0000

    style: height to be viewport height

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -15,8 +15,7 @@ const page = () => {
 			<Header />
 			<div className="bg-darker pb-7 w-full min-h-screen items-center flex relative">
 				<div className="green-blob2 w-96 h-96 top-[-40%] left-[20%] md:top-[-25%] md:left-[10%] animate-pulse"></div>
-
-				<main className="grid grid-cols-1 lg:grid-cols-2 h-auto mt-24 items-center justify-between w-full gap-5 max-w-5xl xl:max-w-6xl my-max">
+				<main className="grid grid-cols-1 lg:grid-cols-2 h-screen mt-24 items-center justify-between w-full gap-5 max-w-5xl xl:max-w-6xl my-max">
 					<SignUpForm />
 					<div className="rounded-[40px] hidden lg:flex overflow-clip">
 						<Image
