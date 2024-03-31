d6eaac7b42645cb381e0f043ac497892bd1643d8
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-03-31 00:14:13 +0000

    refactor:remove module enterpriseSidebar

--- app/dashboard/chat/page.tsx
+++ app/dashboard/chat/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
 import ChatMain from "../(components)/ChatMain";
 import ChatSidebar from "../(components)/ChatSidebar";
-import ChatSidebarEnterprise from "@/app/dashboard-enterprise/(components)/ChatSidebarEnterprise";
 
 export const metadata: Metadata = {
 	title: "SatSat-Ai Chat with your financial data",
