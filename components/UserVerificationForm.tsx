e035b552a882f55bd6019a2a8aabb31f13318a45
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-04-01 07:18:17 +0000

    fix:login request endpoint

--- components/UserVerificationForm.tsx
+++ components/UserVerificationForm.tsx
@@ -45,7 +45,7 @@ const UserVerificationForm = () => {
 		mutationKey: ["resendOneTimeCode"],
 		mutationFn: (email: string) => {
 			const response = axios.post(
-				`${process.env.NEXT_PUBLIC_SATSATAI_MS_USER}/auth/login/request`,
+				`${process.env.NEXT_PUBLIC_SATSATAI_MS_USER}/api/auth/login/request`,
 				{
 					email,
 				}
