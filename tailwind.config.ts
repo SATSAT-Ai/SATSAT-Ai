ace6db7975ce6cfbe58f23ac8da8489c9d3e544b
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-04-01 07:31:18 +0000

    refactor: remove  unused modules

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,12 @@
 import type { Config } from "tailwindcss";
-
-const svgToDataUri = require("mini-svg-data-uri");
-
-const {
-	default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
-
 const config: Config = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/flowbite-react/lib/**/*.js",
 	],
 	theme: {
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "1400px",
-			},
-		},
 		extend: {
 			backgroundImage: {
 				"silver-gradient":
@@ -114,41 +101,10 @@ const config: Config = {
 	},
 
 	plugins: [
-		require("@tailwindcss/aspect-ratio"),
-		addVariablesForColors,
-		function ({ matchUtilities, theme }: any) {
-			matchUtilities(
-				{
-					"bg-grid": (value: any) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-						)}")`,
-					}),
-					"bg-grid-small": (value: any) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-						)}")`,
-					}),
-					"bg-dot": (value: any) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-						)}")`,
-					}),
-				},
-				{ values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-			);
-		},
+		require("flowbite/plugin")({
+			charts: true,
+		}),
 	],
 };
 
-function addVariablesForColors({ addBase, theme }: any) {
-	let allColors = flattenColorPalette(theme("colors"));
-	let newVars = Object.fromEntries(
-		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-	);
-
-	addBase({
-		":root": newVars,
-	});
-}
 export default config;
