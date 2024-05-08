f73695c5a0ab2f4a065a6f593ca4aa5c700a16ef
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-05-08 13:35:47 +0000

    refactor:remove blobs

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -14,7 +14,7 @@ const page = () => {
 		<>
 			<Header />
 			<div className="bg-darker pb-7 w-full min-h-screen items-center flex relative">
-				<div className="green-blob2 w-96 h-96 top-[-40%] left-[20%] md:top-[-25%] md:left-[10%] animate-pulse"></div>
+				<div className="green-blob2 w-96 h-96 top-[-40%] !blur-[150px] left-[20%] md:top-[-25%] md:left-[10%] animate-pulse"></div>
 				<main className="grid grid-cols-1 lg:grid-cols-2 mt-24 h-full items-center justify-between w-full gap-5 max-w-5xl xl:max-w-6xl my-max">
 					<SignUpForm />
 					<div className="rounded-[40px] hidden lg:flex overflow-clip">
