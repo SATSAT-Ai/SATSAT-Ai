07a9a1538817b8d4349fbebf349cb0e8b4f930c8
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-05-08 12:10:13 +0000

    feat:custom success page after paddle payment

---
+++ components/PaymentSuccess.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const PaymentSuccess = () => {
+	return (
+		<main className="w-full grid place-content-center bg-darker text-white h-screen">
+			<div className="">payment successful</div>
+		</main>
+	);
+};
+
+export default PaymentSuccess;
