4d126e7c69e938bd08e71e00fab0c2caca0a8968
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-05-08 13:18:44 +0000

    refactor:fetch user transactions server-side

--- app/dashboard/transactions/page.tsx
+++ app/dashboard/transactions/page.tsx
@@ -1,11 +1,37 @@
 import { Metadata } from "next";
 import TransactionsClientPage from "../(components)/TransactionsClientPage";
+import axios from "axios";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
 export const metadata: Metadata = {
 	title: "SatSat-Ai Transactions",
 };
 
-const Page = () => {
+const apiUrl = process.env.SATSATAI_MS_STATEMENT;
+const getUserTransactions = async () => {
+	const session = await getServerSession(options);
+
+	if (session && session.user.access_token) {
+		try {
+			const response = axios.get(
+				`${apiUrl}/api/mobile-money-transactions/index`, //?per_page=1
+				{
+					headers: {
+						Authorization: `Bearer ${session.user.access_token}`,
+					},
+				}
+			);
+			const data = await (await response).data;
+			return data;
+		} catch (error: any) {
+			console.log(error);
+		}
+	}
+};
+const Page = async () => {
+	// const transactions = await getUserTransactions();
+	// console.log(transactions);
 	return <TransactionsClientPage />;
 };
 
