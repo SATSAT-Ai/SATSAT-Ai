1d278420cb03c64f9e6cfc44708258f510692290
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-05-08 12:45:08 +0000

    remove: topBanner context from QueryProvider

--- components/QueryProvider.tsx
+++ components/QueryProvider.tsx
@@ -2,39 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import {
-	Dispatch,
-	ReactNode,
-	SetStateAction,
-	createContext,
-	useState,
-} from "react";
-import EarlyAccessModal from "./ui/EarlyAccessModal";
-
-interface Ibanner {
-	showEarlyAccessModal: boolean;
-	setShowEarlyAccessModal: Dispatch<SetStateAction<boolean>>;
-}
-
-export const bannerContext = createContext<Ibanner>({
-	setShowEarlyAccessModal: () => {},
-	showEarlyAccessModal: false,
-});
+import { ReactNode } from "react";
 
 const QueryProvider = ({ children }: { children: ReactNode }) => {
-	const [showEarlyAccessModal, setShowEarlyAccessModal] = useState(false);
-
 	const queryClient = new QueryClient();
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<bannerContext.Provider
-				value={{ showEarlyAccessModal, setShowEarlyAccessModal }}
-			>
-				{children}
-
-				{showEarlyAccessModal && <EarlyAccessModal />}
-			</bannerContext.Provider>
+			{children}
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	);
