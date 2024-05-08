be98beb7232b3f04492f8bbd22c9bdcb2a675d7d
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-05-08 12:42:01 +0000

    feat:determine billing_period when plan is subScribed

--- components/SubscribeButton.tsx
+++ components/SubscribeButton.tsx
@@ -2,14 +2,18 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { HTMLAttributes } from "react";
 
-type SubScribeButton = HTMLAttributes<HTMLButtonElement> & {
+export type billing_period = "annual" | "monthly";
+
+export type SubScribeButton = HTMLAttributes<HTMLButtonElement> & {
 	name: string;
 	buttonType: string;
+	billing_period: billing_period;
 };
 
 const SubscribeButton = ({
 	name,
 	buttonType,
+	billing_period,
 	...restProps
 }: SubScribeButton) => {
 	const router = useRouter();
@@ -18,6 +22,7 @@ const SubscribeButton = ({
 
 	const handlePlan = () => {
 		params.set("plan", buttonType);
+		params.set("period", billing_period);
 		if (buttonType === "enterprise") {
 			router.replace(`/contact?${params}`);
 		} else {
