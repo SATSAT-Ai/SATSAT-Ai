dff88c853b5910ff60a161d6b504889b4d7e8b63
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-05-08 12:28:32 +0000

    refactor:remove close button from topbanner

--- components/ui/TopBanner.tsx
+++ components/ui/TopBanner.tsx
@@ -1,31 +1,53 @@
 "use client";
 
-import { useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import { MdClose } from "react-icons/md";
-import { bannerContext } from "../QueryProvider";
+import Link from "next/link";
 
 const TopBanner = () => {
-	const [showBanner, setShowBanner] = useState(true);
-	const { setShowEarlyAccessModal } = useContext(bannerContext);
+	const [showBanner, setShowBanner] = useState<boolean | null>(false);
+
+	useEffect(() => {
+		//show closed banner every 24hrs
+
+		const expirationDuration = 24 * 60 * 60 * 1000; // 24 hours
+		const hasExpired = new Date().getTime() - expirationDuration > 0; // Check if 24 hrs passed
+
+		const bannerInSession = JSON.parse(
+			localStorage.getItem("showBanner") as unknown as string
+		);
+		if (bannerInSession !== false && !hasExpired) {
+			setShowBanner(false);
+		}
+		localStorage.setItem("showBanner", JSON.stringify(true));
+		setShowBanner(bannerInSession);
+	}, []);
+
+	const handleShowBanner = () => {
+		localStorage.setItem("showBanner", JSON.stringify(false));
+		setShowBanner(false);
+	};
 
 	return (
-		showBanner && (
+		showBanner !== false && (
 			<div className="text-white flex items-center gap-5 px-5 py-2 justify-between w-full text-center bg-darker">
 				<p className="flex-[11] font-medium text-text-14 sm:text-text-normal">
 					To Get Early Access To SatSat Ai, Please{" "}
-					<button
-						onClick={() => setShowEarlyAccessModal(true)}
+					<Link
+						data-test="topBanner"
+						target="_blank"
+						href="https://forms.gle/CbDpLjhDCdx1NugU8"
 						className="text-brand-green font-medium underline text-text-14 sm:text-text-normal"
 					>
 						Join The Waitlist
-					</button>{" "}
+					</Link>{" "}
 					✨
 				</p>
-				<MdClose
+				{/* <MdClose
 					color="white"
 					className="cursor-pointer active:scale-[1.02] text-text-24 transition-colors duration-100 hover:bg-grey-light rounded-full p-1"
-					onClick={() => setShowBanner(false)}
-				/>
+					onClick={() => handleShowBanner()}
+				/> */}
 			</div>
 		)
 	);
