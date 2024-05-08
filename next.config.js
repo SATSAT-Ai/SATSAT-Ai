ca5863024356ef10eb5fa0f5b174a68fb25c5842
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-05-08 12:48:54 +0000

    refactor:enable pwa on development server

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const withPWA = require("next-pwa");
 module.exports = withNextra(
 	withPWA({
 		pwa: {
-			// disable: process.env.NODE_ENV === "development",
+			disable: process.env.NODE_ENV === "development",
 			dest: "public",
 			register: true,
 			skipWaiting: true,
