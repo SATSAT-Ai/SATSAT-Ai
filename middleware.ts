e0f1ae9d075f34782a2100fd29f9aae8cf1b8cb6
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-05-08 13:13:05 +0000

    feat:authorize users

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,18 @@ export default withAuth(
 	function middleware(request: NextRequestWithAuth) {
 		if (
 			request.nextUrl.pathname.startsWith("/dashboard") &&
-			request.nextauth.token?.role !== "admin"
+			request.nextauth.token?.role !== "admin" // replace with (request.nextauth.token?.role !== "GOD" || request.nextauth.token?.role !== "Partner")
 		) {
-			return NextResponse.rewrite(new URL("/denied", request.url));
+			// return NextResponse.rewrite(new URL("/denied", request.url));
 		}
+
+		// if (request.nextUrl.pathname.startsWith("/dashboard")) {
+		// 	if (request.nextauth.token?.role === "GOD" || request.nextauth.token?.role === "Partner") {
+		// 	  return NextResponse.rewrite(new URL("/dashboard-enterprise", request.url));
+		// 	} else {
+		// 	  return NextResponse.rewrite(new URL("/dashboard", request.url));
+		// 	}
+		//   }
 	},
 
 	{
