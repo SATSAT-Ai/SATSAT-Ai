1cda83966ab7e400a354cafd808f9dd44f2eb9c6
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-05-08 13:00:40 +0000

    refactor:add classVariance to Feature card

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -1,19 +1,29 @@
+import { cn } from "@/lib/utils";
+
 const Feature = ({
 	title,
 	icon,
 	para,
+	className,
 }: {
 	title: string;
 	para: string;
 	icon: any;
+	className?: string;
 }) => {
 	return (
-		<div className=" relative text-white p-2 bg-[#071f07] rounded-2xl">
+		<div
+			className={cn(
+				"relative text-white p-3 bg-[#071f07] rounded-lg",
+				className
+			)}
+		>
 			{icon}
+
 			<h2 className="text-text-20 font-medium mt-2">{title}</h2>
 			<p
 				data-test={"active-feature-text"}
-				className="mb-3 mt-1 text-white/80 font-normal text-text-normal"
+				className="mb-3 mt-1 text-white/70 font-normal text-text-normal"
 			>
 				{para}
 			</p>
